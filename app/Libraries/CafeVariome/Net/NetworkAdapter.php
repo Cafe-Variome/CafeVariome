180c6b1a19ccf13b995569a63449f137d5bdbb02
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-10-31 17:28:50 +0000

    Created NetworkAdapter class.
    Base class for network communications.

---
+++ app/Libraries/CafeVariome/Net/NetworkAdapter.php
@@ -0,0 +1,19 @@
+<?php namespace App\Libraries\CafeVariome\Net;
+
+/**
+ * NetworkAdapter.php
+ * Created: 15/10/2019
+ * 
+ * @author Mehdi Mehtarizadeh
+ * 
+ */
+
+class NetworkAdapter  implements INetworkAdapter
+{
+    protected $adapterInstance;
+
+    public function Send()
+    {
+        $this->$adapterInstance->Send();
+    }
+}
