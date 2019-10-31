3b8cdaeb2f6a29a379334a96f4c9cd2445d6f1b6
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-10-31 17:30:34 +0000

    cURLAdapter class created.
    Extends NetworkAdapter and uses cURL as the adapter instance.

---
+++ app/Libraries/CafeVariome/Net/cURLAdapter.php
@@ -0,0 +1,60 @@
+<?php namespace App\Libraries\CafeVariome\Net;
+
+/**
+ * cURLRequest.php
+ * 
+ * Created: 15/10/2019
+ * 
+ * @author Mehdi Mehtarizadeh
+ * 
+ */
+
+ class cURLAdapter extends NetworkAdapter 
+ {  
+    protected $adapterInstance;
+
+    private $baseUrl;
+
+    public function __construct(string $url = null, array $option_values = null) {
+
+        if ($this->curlEnabled()) {
+            if ($url) {
+                $this->baseUrl = $url;
+                $this->adapterInstance = curl_init($this->baseUrl);
+            }
+            else {
+                $this->adapterInstance = curl_init();
+            }
+
+            if ($option_values && count($option_values) > 0) {
+                foreach ($option_values as $option => $value) {
+                    $this->setOption($option, $value);
+                }
+            }
+        }
+        else {
+            throw new \Exception('cURL is not installed or enabled.');
+        }
+        
+    }
+
+    public function setOption(int $option, $value): bool
+    {
+        return curl_setopt($this->adapterInstance, $option, $value);
+    }
+
+    public function Send(): mixed
+    {
+        return curl_exec($this->adapterInstance);
+    }
+
+    private function curlEnabled(): bool
+    {
+        return function_exists('curl_version');
+    }
+    
+    function __destruct() {
+        curl_close($this->adapterInstance);
+    }
+ }
+ 
