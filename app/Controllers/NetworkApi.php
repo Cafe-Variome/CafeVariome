5cceba6fded5078c1a31c67ec93aa3c29dc14136
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-11-14 15:46:32 +0000

    Created NetworkApi endpoint.
    This controller listens to and handles requests from authentication server and other installations.

---
+++ app/Controllers/NetworkApi.php
@@ -0,0 +1,21 @@
+<?php namespace App\Controllers;
+
+/**
+ * NetworkApi.php
+ * 
+ * Created : 01/10/2019
+ * 
+ * @author Gregory Warren
+ * @author Mehdi Mehtarizadeh
+ * 
+ * This controller contains RESTful listeners for network operations. 
+ * Most methods are ported from netauth.php in the previous version.
+ */
+
+use CodeIgniter\RESTful\ResourceController;
+use App\Helpers\NetworkApiHelper;
+
+class NetworkApi extends ResourceController{
+
+
+}
