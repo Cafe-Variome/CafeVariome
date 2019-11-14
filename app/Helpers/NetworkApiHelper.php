ec8fb01b22479f7ec0196cab121cddd89a528fed
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-11-14 15:47:16 +0000

    NetworkApiHelper created.
    This helper contains functions that interact with the database and other parts of the application.

---
+++ app/Helpers/NetworkApiHelper.php
@@ -0,0 +1,19 @@
+<?php namespace App\Helpers;
+
+/**
+ * NetworkApiHelper.php
+ * 
+ * Created: 02/10/2019
+ * 
+ * @author Mehdi Mehtarizadeh
+ * 
+ * This is a helper class for the RESTful api used among installations.
+ *  
+ */
+
+use App\Models\User;
+
+class NetworkApiHelper 
+{
+    
+}
