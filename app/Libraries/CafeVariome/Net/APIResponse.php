2f8ef5b26c1fb6aa258983155df5fb75a7d90c9e
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-12-02 15:34:22 +0000

    APIResponse added.

---
+++ app/Libraries/CafeVariome/Net/APIResponse.php
@@ -0,0 +1,40 @@
+<?php namespace App\Libraries\Net;
+
+/**
+ * APIResponse.php
+ * Created: 19/11/2019
+ * @author Mehdi Mehtarizadeh
+ * 
+ * This entity class forms the basic data structure for responses in API requests.
+ * 
+ */
+
+class APIResponse
+{
+    private $status;
+    private $message;
+
+    public function __construct(int $status = null) {
+        $this->status = $status;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status)
+    {
+        $this->status = $status;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+    }
+}
