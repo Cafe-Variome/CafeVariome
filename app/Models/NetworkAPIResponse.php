84c634a126d798799405e60f50bec37620511ce5
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2019-12-19 17:15:51 +0000

    NetworkAPIResponse added.

---
+++ app/Models/NetworkAPIResponse.php
@@ -0,0 +1,76 @@
+<?php namespace App\Models;
+
+/**
+ * NetworkAPIResponse.php
+ * Created: 19/11/2019
+ * 
+ * @author Mehdi Mehtarizadeh
+ * 
+ * Response structure for Network model operations.
+ * 
+ */
+
+use App\Libraries\CafeVariome\Net\APIResponse;
+
+class NetworkAPIResponse extends APIResponse
+{
+    private $status;
+    private $message;
+
+    private $data;
+
+    public function __construct(int $status = null, array $data = null) {
+        parent::__construct($status);
+
+        $this->status = $status;
+
+        switch ($status) {
+            case 0:
+                $this->message = 'Operation failed.';
+                break;
+            case 1:
+                $this->message = 'Operation was successful.';
+                break;           
+            default:
+                break;
+        }
+
+        $this->data = $data;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status)
+    {
+        $this->status = $status;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+    }
+
+    public function toArray(): array
+    {
+        $responseArray = [
+            'status' => $this->status,
+            'message' => $this->message,
+            'data' => $this->data
+        ];
+
+        return $responseArray;
+    }
+
+    public function toJSON()
+    {
+        return json_encode($this->toArray());
+    }
+}
