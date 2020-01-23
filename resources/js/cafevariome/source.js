9961137aa655a6e00e71edd4d548ff060fff2b38
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2020-01-23 10:03:50 +0000

    Added tooltip for "import records" button. VCF Import link added to modal.

--- resources/js/cafevariome/source.js
+++ resources/js/cafevariome/source.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
+    // If the table exists, populate DataTable and make it stylish.
     if ($('#sourcestable').length) {
         $('#sourcestable').DataTable();
     }
+    $('#ImportRecordsBtn').tooltip(); //Tooltip for import records added as the data-toggle attribute is used to trigger the modal
 } );
+
+// Select groups on submission of form
 function select_groups() {
     $(".groupsSelected").find('option').each(function () {
         $(this).attr('selected', 'selected');
     });
 }
 
+// Set proper hyperlinks to uploaders in the modal
 $('#addVariantsModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget); 
     var sourceId = button.data('id'); 
@@ -19,5 +24,6 @@ $('#addVariantsModal').on('show.bs.modal', function (event) {
     modal.find('.modal-title').text('Add Records To ' + sourceDesc);
     modal.find('#bulkImport').attr('href', baseurl + "Upload/Bulk/" + sourceId);
     modal.find('#phenoPacketsImport').attr('href', baseurl + "Upload/Json/" + sourceId);
+    modal.find('#VCFImport').attr('href', baseurl + "Upload/VCF/" + sourceId);
   })
 
