5faf9e52560eb1c2ea0f48ba5329bba8dacb1bf9
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2020-02-17 12:23:26 +0000

    Replaced master with dashboard in all views.

--- app/Views/Source/Create.php
+++ app/Views/Source/Create.php
@@ -1,12 +1,6 @@
-<?= $this->extend('layout/master') ?>
+<?= $this->extend('layout/dashboard') ?>
 <?= $this->section('content') ?>
-<nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
-  	<li class="breadcrumb-item"><a href="<?php echo base_url('admin/index');?>">Dashboard Home</a></li>
-  	<li class="breadcrumb-item"><a href="<?php echo base_url($controllerName);?>">Sources</a></li>
-    <li class="breadcrumb-item active" aria-current="page"><?= $title ?></li>
-  </ol>
-</nav>
+
 <div class="row">
 	<div class="col">
 		<h2><?= $title ?></h2>	
