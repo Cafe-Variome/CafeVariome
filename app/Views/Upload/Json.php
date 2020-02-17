04ba4a4c0d79ec98f14de00de359e6703d89a22f
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2020-02-17 17:50:49 +0000

    Navbar removed from uploader views.

--- app/Views/Upload/Json.php
+++ app/Views/Upload/Json.php
@@ -3,9 +3,9 @@
 
 <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="<?= base_url('Admin/Index') ?>">Dashboard Home</a></li>
-    <li class="breadcrumb-item"><a href="<?= base_url('Source') ?>">Sources</a></li>
-    <li class="breadcrumb-item active" aria-current="page"><?= $title ?></li>
+	<li class="breadcrumb-item"><a href="<?= base_url('Admin/Index') ?>">Dashboard Home</a></li>
+	<li class="breadcrumb-item"><a href="<?= base_url('Source') ?>">Sources</a></li>
+	<li class="breadcrumb-item active" aria-current="page"><?= $title ?></li>
   </ol>
 </nav>
 <div class="row">
@@ -16,52 +16,52 @@
 <hr>
 <div id="load"></div>
 <div class="row">
-    <div class="col">
-        <h4>Import jsons into <?php echo $source_name; ?></h3>
-        <p>This is built to only work with PhenoPackets. </p>
-        <p>Up to 200 files can be uploaded in each batch. If you have more files to upload please break your upload into chunks.</p>
+	<div class="col">
+		<h4>Import jsons into <?php echo $source_name; ?></h3>
+		<p>This is built to only work with PhenoPackets. </p>
+		<p>Up to 200 files can be uploaded in each batch. If you have more files to upload please break your upload into chunks.</p>
 
-    </div>
+	</div>
 </div>
 
 <div id="json_errors"></div>
 <form enctype="multipart/form-data" method="post" id="phenoinfo">
-    <input type="hidden" name="user_id" id="user_id" value="<?= $user_id ?>" />
-    <label>File/Files to submit:</label>
-    <span class="btn btn-default btn-file" style="padding-top: 10px; line-height: normal;">
-        <input type="file" name="userfile[]" id="jsonFile" multiple required/>
-    </span>
-    <input type="hidden" id="source_id" value="<?php echo $source_id ?>" />
-    <br/><br/>
-    <div class="pagination-centered">
-        <button class="span3 btn btn-large btn-primary " type="submit">Upload File</button>
-    </div>
+	<input type="hidden" name="user_id" id="user_id" value="<?= $user_id ?>" />
+	<label>File/Files to submit:</label>
+	<span class="btn btn-default btn-file" style="padding-top: 10px; line-height: normal;">
+		<input type="file" name="userfile[]" id="jsonFile" multiple required/>
+	</span>
+	<input type="hidden" id="source_id" value="<?php echo $source_id ?>" />
+	<br/><br/>
+	<div class="pagination-centered">
+		<button class="span3 btn btn-large btn-primary " type="submit">Upload File</button>
+	</div>
 </form> 
 <hr>
 <div class="row">
-    <div class="col">
-        <p>The Status table will refresh every 5 seconds. However as long as the search box is highlighted the refresh will not occur.</p>
-    </div>
+	<div class="col">
+		<p>The Status table will refresh every 5 seconds. However as long as the search box is highlighted the refresh will not occur.</p>
+	</div>
 </div>
 <table class="table table-hover table-striped" id="file_table">
-    <thead>
-        <tr>
-        <th>File-name</th>
-        <th>User</th>
-        <th>Upload Start</th>
-        <th>Upload End</th>
-        <th>Errors</th>
-        <th>Status</th>
-        </tr>
-    </thead>
-    <tbody id="file_grid">
-    </tbody>
+	<thead>
+		<tr>
+		<th>File-name</th>
+		<th>User</th>
+		<th>Upload Start</th>
+		<th>Upload End</th>
+		<th>Errors</th>
+		<th>Status</th>
+		</tr>
+	</thead>
+	<tbody id="file_grid">
+	</tbody>
 </table>
 <hr>
 <div class="row">
-    <div class="col">
-        <a class="btn btn-secondary" href="<?= base_url('Source') ?>"><i class="fa fa-backward"></i> Go back</a>
-    </div>
+	<div class="col">
+		<a class="btn btn-secondary" href="<?= base_url('Source') ?>"><i class="fa fa-backward"></i> Go back</a>
+	</div>
 </div>
 
 <?= $this->endSection() ?>
