8d35fc7c3ea881c88bc4675a4f1b0c78773eee0f
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2020-03-05 13:50:38 +0000

    Removed navbar.

--- app/Views/Discover/Query_Builder.php
+++ app/Views/Discover/Query_Builder.php
@@ -1,11 +1,5 @@
 <?= $this->extend('layout/master') ?>
 <?= $this->section('content') ?>
-<nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
-  <li class="breadcrumb-item"><a href="<?php echo base_url() . "admin/index";?>">Dashboard Home</a></li>
-    <li class="breadcrumb-item active" aria-current="page"><?= $title ?></li>
-  </ol>
-</nav>
 <div class="row">
 	<div class="col">
 		<h2><?= $title ?></h2>	
