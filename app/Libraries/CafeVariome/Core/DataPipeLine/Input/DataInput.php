5718ecd8b01d4788339b175b0d8c21905a326912
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2020-03-24 00:45:32 +0000

    DataInput class created.

---
+++ app/Libraries/CafeVariome/Core/DataPipeLine/Input/DataInput.php
@@ -0,0 +1,15 @@
+<?php namespace App\Libraries\CafeVariome\Core\DataPipeLine\Stream;
+
+/**
+ * Name DataInput.php
+ * 
+ * Created 11/03/2020
+ * @author Mehdi Mehtarizadeh
+ * @author Gregory Warren
+ * 
+ */
+
+class DataInput
+{
+    
+}
