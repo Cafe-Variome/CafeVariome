1c8661c164fa49c4443834db78707e4326572fc3
Mehdi Mehtarizadeh <mehdi_mehtarizade@hotmail.com>
2020-05-06 14:13:49 +0100

    Apache user put into variable.

--- Install/install.sh
+++ Install/install.sh
@@ -1,17 +1,19 @@
 cat Install/cv.txt
 
+apacheUser = "www-data"
+
 echo "Welcome to Cafe Variome Installer"                                                               
 echo "Cafe Variome is produced at BrookesLab, University of Leicester."                                                               
 echo ""                                                               
 echo "Setting directory permissions..."                                                                   
-setfacl -m u:www-data:rwx writable/
-setfacl -m u:www-data:rwx writable/logs/
-setfacl -m u:www-data:rwx writable/cache/
-setfacl -m u:www-data:rwx writable/uploads/
-setfacl -m u:www-data:rwx writable/session/
-setfacl -m u:www-data:rwx resources/phenotype_lookup_data/
-setfacl -m u:www-data:rwx upload/
-setfacl -m u:www-data:rwx upload/Pairings/
-setfacl -m u:www-data:rwx upload/UploadData/
+setfacl -m u:$apacheUser:rwx writable/
+setfacl -m u:$apacheUser:rwx writable/logs/
+setfacl -m u:$apacheUser:rwx writable/cache/
+setfacl -m u:$apacheUser:rwx writable/uploads/
+setfacl -m u:$apacheUser:rwx writable/session/
+setfacl -m u:$apacheUser:rwx resources/phenotype_lookup_data/
+setfacl -m u:$apacheUser:rwx upload/
+setfacl -m u:$apacheUser:rwx upload/Pairings/
+setfacl -m u:$apacheUser:rwx upload/UploadData/
 echo "Directory permissions set."                                                                   
 
