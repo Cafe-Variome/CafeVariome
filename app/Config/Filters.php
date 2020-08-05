e377f4bc0ebf532be9bd2e0e8f958e6c4d0dd47c
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2020-08-05 13:59:23 +0100

    CSRF exception added.

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -16,7 +16,7 @@ class Filters extends BaseConfig
 	public $globals = [
 		'before' => [
 			//'honeypot'
-		    'csrf',
+		    'csrf' => ['except' => ['QueryApi/Query']]
 		],
 		'after'  => [
 			//'toolbar',
