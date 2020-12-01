a09f3f684232ca56e71499725fcaab7f38b813a1
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2020-12-01 15:39:34 +0000

    Disabled horizontal scroll for users data table.

--- resources/js/cafevariome/admin.js
+++ resources/js/cafevariome/admin.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     if ($('#userstable').length) {
-        $('#userstable').DataTable({"scrollX": true });
+        $('#userstable').DataTable(/*{"scrollX": true }*/);
     }
 } );
