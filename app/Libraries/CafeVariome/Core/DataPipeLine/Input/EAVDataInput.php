978b3626f46be6f63fabbf33c8a67d75b52a018b
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-02-19 14:43:10 +0000

    Added function calls to communicate with ServiceInterface.

--- app/Libraries/CafeVariome/Core/DataPipeLine/Input/EAVDataInput.php
+++ app/Libraries/CafeVariome/Core/DataPipeLine/Input/EAVDataInput.php
@@ -9,18 +9,18 @@
  * 
  */
 
-use App\Models\Upload;
-use App\Models\Source;
-use App\Models\EAV;
+use App\Libraries\CafeVariome\Net\ServiceInterface;
 
 class EAVDataInput extends DataInput
 {
     private $delete;
     private $error;
+    private $serviceInterface;
 
     public function __construct(int $source_id, int $delete) {
         parent::__construct($source_id);
         $this->delete = $delete;
+        $this->serviceInterface = new ServiceInterface();
     }
 
     public function absorb(int $file_id){
@@ -77,16 +77,20 @@ class EAVDataInput extends DataInput
         
                 $this->sourceModel->toggleSourceLock($this->sourceId);	
                 $this->reader->open($filePath);
-
             }
         }
     }
 
-    public function save(int $file_id){
+    public function save(int $file_id)
+    { 
+        $recordCount = $this->countRecords();
+        $this->serviceInterface->RegisterProcess($file_id, $recordCount, 'bulkupload');
 
         list($true, $linerow, $counter, $groupnumber) = array(true, 1, 0, 0);
 
         foreach ($this->reader->getSheetIterator() as $sheet) {
+            $recordsProcessed = -1;  // set counter to -1 initially to avoid counting header of the file
+
             foreach ($sheet->getRowIterator() as $row) {
                 if ($true) {			 
                     for ($i=0; $i < count($row); $i++) { 	
@@ -155,17 +159,26 @@ class EAVDataInput extends DataInput
                         }
                     }
                 }
+                $recordsProcessed++;
+
+                $this->serviceInterface->ReportProgress($file_id, $recordsProcessed, $recordCount, 'bulkupload');
             }
+
             $linerow++;
         }
 
         $this->reader->close();
+
+        $totalRecordCount = $this->sourceModel->countSourceEntries($this->sourceId);
+
+        $this->sourceModel->updateSource(['record_count' => $totalRecordCount], ['source_id' => $this->sourceId]);
         
         $this->db->transComplete();
         $this->uploadModel->insertStatistics($file_id, $this->sourceId);
         $this->uploadModel->bigInsertWrap($file_id, $this->sourceId);
         $this->uploadModel->clearErrorForFile($file_id);
         $this->sourceModel->toggleSourceLock($this->sourceId);	
+
     }
 
     /**
@@ -184,4 +197,33 @@ class EAVDataInput extends DataInput
            $this->db->transStart();
        }
     }
+
+    private function countRecords(): int
+    {
+        $rc = -1; // set counter to -1 initially to avoid counting header of the file
+        foreach ($this->reader->getSheetIterator() as $sheet) {
+            foreach ($sheet->getRowIterator() as $row) {
+                $rc++;
+            }
+        }
+        return $rc;
+    }
+
+    // private function report(array $report)
+    // {
+    //     $address = "127.0.0.1";
+    //     $service_port = "49200";
+
+    //     $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+    //     $result = socket_connect($socket, $address, $service_port);
+    
+    //     socket_write($socket, json_encode($report), strlen(json_encode($report)));
+    //     while ($out = socket_read($socket, 2048)) {
+    //         echo $out;
+    //     }
+    //     socket_close($socket);
+    
+    //     usleep(100);
+    // }
 }
