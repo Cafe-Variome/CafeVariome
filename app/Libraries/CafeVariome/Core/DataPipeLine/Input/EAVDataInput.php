31597c2dad51f25d696be15f5b6be33e6ad17c74
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-04-21 11:21:46 +0100

    Added configuration array to the EAVDataInput.

--- app/Libraries/CafeVariome/Core/DataPipeLine/Input/EAVDataInput.php
+++ app/Libraries/CafeVariome/Core/DataPipeLine/Input/EAVDataInput.php
@@ -16,11 +16,18 @@ class EAVDataInput extends DataInput
     private $delete;
     private $error;
     private $serviceInterface;
+    private $configuration;
+    private $column_count;
 
     public function __construct(int $source_id, int $delete) {
         parent::__construct($source_id);
         $this->delete = $delete;
         $this->serviceInterface = new ServiceInterface();
+
+        $this->configuration = ['subject_id_column' => 'pseudonym',
+                                'grouping_policy' => 0, // 0 -> separate all columns with <group_end>, 1 -> custom: define <group_end> positions
+                                'group_end_positions' => []
+        ];
     }
 
     public function absorb(int $file_id){
@@ -50,12 +57,12 @@ class EAVDataInput extends DataInput
                 $attgroups = [];
                 if (preg_match("/\.csv$|\.tsv$/", $file)) {
                     $line = fgets(fopen($filePath, 'r'));
-                    if (!preg_match("/^subject_id(.)/", $line, $matches)) {
+                    if (!preg_match("/^" . $this->configuration['subject_id_column'] . "(.)/", $line, $matches)) {
+                        $message = "No " . $this->configuration['subject_id_column'] . " column.";
                         $return_data['result_flag'] = 0;
-                        $return_data['error'] = 'No subject_id column.';
-                        $message = 'No subject_id column.';
+                        $return_data['error'] = $message;
                         $error_code = 1;
-                        $this->uploadModel->errorInsert($fileId,$this->sourceId,$message,$error_code,true);
+                        $this->uploadModel->errorInsert($fileId, $this->sourceId, $message, $error_code, true);
                         return $return_data;
                     }
                     else {
@@ -76,7 +83,7 @@ class EAVDataInput extends DataInput
                     $return_data['error'] = "File did not conform to allowed types";   
                     $message = "File did not conform to allowed types.";
                     $error_code = 2;
-                    $this->uploadModel->errorInsert($fileId,$message,$error_code,true);         
+                    $this->uploadModel->errorInsert($fileId, $message, $error_code, true);         
                     return $return_data;
                 }
         
@@ -90,6 +97,7 @@ class EAVDataInput extends DataInput
         $this->serviceInterface->ReportProgress($file_id, 0, 1, 'bulkupload', 'Counting records');
 
         $recordCount = $this->countRecords();
+        $group_positions = $this->getGroupPositions();
 
         $this->serviceInterface->ReportProgress($file_id, 0, $recordCount, 'bulkupload', 'Importing data');
 
@@ -98,32 +106,32 @@ class EAVDataInput extends DataInput
         foreach ($this->reader->getSheetIterator() as $sheet) {
             $recordsProcessed = -1;  // set counter to -1 initially to avoid counting header of the file
 
+            $temphash = [];
             foreach ($sheet->getRowIterator() as $row) {
                 if ($true) {			 
                     for ($i=0; $i < count($row); $i++) { 	
                         if ($i === 0) {
-                            if ($row[$i] != "subject_id"){
+                            if ($row[$i] != $this->configuration['subject_id_column'] /*"subject_id"*/){
+                                $message = "No " . $this->configuration['subject_id_column'] . " column.";
                                 $return_data['result_flag'] = 0;
-                                $return_data['error'] = "No subject_id column.";
-                                $message = "No subject_id column.";
+                                $return_data['error'] = $message;
                                 $error_code = 1;
-                                $this->uploadModel->errorInsert($file_id,$this->sourceId,$message,$error_code,true);
+                                $this->uploadModel->errorInsert($file_id, $this->sourceId, $message, $error_code, true);
                                 $this->sourceModel->unlockSource($this->sourceId);
                                 return $return_data;
                             }
                             continue;
-                        }                    
-                        if ($row[$i] == "<group_end>"){
-                            if (!empty($temphash)){
+                        }
+                        $temphash[$row[$i]] = $i;
+                    
+                        if (in_array($i , $group_positions)){
+                            if (count($temphash) > 0){
                                 $attgroups[$groupnumber] = $temphash;
-                                $temphash = [];
                                 $groupnumber++;
+                                $temphash = [];
                             }
-                        } 
-                        else {
-                            $temphash[$row[$i]] = $i;
-                        }                      
-                        if (!empty($temphash)){
+                        }                 
+                        if (count($temphash) > 0){
                             $attgroups[$groupnumber] = $temphash;
                         }	
                     }
@@ -139,7 +147,7 @@ class EAVDataInput extends DataInput
                         $return_data['error'] = "All records require a record ID, a record on line:".$linerow." in the import data that do not have a record ID, please add record IDs to all records and re-try the import.";
                         $message = "All records require a record ID, a record on line:".$linerow." in the import data that do not have a record ID, please add record IDs to all records and re-try the import.";
                         $error_code = 3;
-                        $this->uploadModel->errorInsert($file_id,$this->sourceId,$message,$error_code,true);
+                        $this->uploadModel->errorInsert($file_id, $this->sourceId, $message, $error_code, true);
                         $this->sourceModel->unlockSource($this->sourceId);
                         return $return_data;
                     }
@@ -149,7 +157,7 @@ class EAVDataInput extends DataInput
                             $value = $row[$val];
                             if ($value == "") continue;     
                             if (is_a($value, 'DateTime')) $value = $value->format('Y-m-d H:i:s');
-                            $this->uploadModel->jsonInsert($uid,$this->sourceId,$file_id,$subject_id,$att,$value);
+                            $this->uploadModel->jsonInsert($uid, $this->sourceId, $file_id, $subject_id, $att, $value);
                             $counter++;                            
                             if ($counter % 800 == 0) {
                                 $error = $this->sendBatch();   
@@ -187,8 +195,6 @@ class EAVDataInput extends DataInput
         $this->sourceModel->unlockSource($this->sourceId);	
 
         $this->serviceInterface->ReportProgress($file_id, 1, 1, 'bulkupload', 'Finished', true);
-
-
     }
 
     /**
@@ -213,9 +219,27 @@ class EAVDataInput extends DataInput
         $rc = -1; // set counter to -1 initially to avoid counting header of the file
         foreach ($this->reader->getSheetIterator() as $sheet) {
             foreach ($sheet->getRowIterator() as $row) {
+                $this->column_count = count($row);
                 $rc++;
             }
         }
+        
         return $rc;
+    }
+
+    private function getGroupPositions(): array
+    {
+        return $this->configuration['grouping_policy'] == 0 ? $this->getDefaultGroupPositions() : $this->configuration['group_end_positions'];
+    }
+
+    private function getDefaultGroupPositions(): array
+    {
+        $positions = [];
+
+        for ($i=1; $i < $this->column_count; $i++) { 
+            $positions[$i] = $i;
+        }
+
+        return $positions;
     }
 }
