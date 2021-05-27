51220f1e8cc2d9491c3dd23f9b1c5c57096f5df6
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-05-27 11:55:31 +0100

    Added recursive lookup feature to SysFileMan.

--- app/Libraries/CafeVariome/Core/IO/FileSystem/SysFileMan.php
+++ app/Libraries/CafeVariome/Core/IO/FileSystem/SysFileMan.php
@@ -13,26 +13,72 @@
  {
     protected $basePath;
     protected $files;
+    private $extension_filter;
 
-    public function __construct(string $basePath = null) {
+    public function __construct(string $basePath = null, bool $recursive = false, array $extension_filter = []) {
         parent::__construct($basePath);
 
         $this->files = [];
-        $this->loadFiles();
+        $this->extension_filter = $this->toLowerCase($extension_filter);
+        $this->loadFiles('', $recursive);
     }
 
-    private function loadFiles(string $path = '')
+    private function toLowerCase(array $arr): array
     {
-        if ($this->Exists($path)) {
-            $this->files = scandir($this->getFullPath() . $path);
+        $new_arr = [];
+        foreach ($arr as $element) {
+            $new_arr[] = strtolower($element);
+        }
+        return $new_arr;
+    }
+
+    private function loadFiles(string $path = '', bool $recursive = false)
+    {
+        if(!$recursive){
+            if ($this->Exists($path)) {
+                if (is_dir($this->getFullPath() . $path)) {
+                    $this->files = scandir($this->getFullPath() . $path);
+                }
+            }
+        }
+        else{
+            $cdir = @scandir($this->getFullPath() . $path);
+            if (is_array($cdir)) {
+
+                foreach ($cdir as $dir)
+                {
+                    if (!in_array($dir, [".",".."]))
+                    {
+                        if (is_dir($this->getFullPath() . $path . DIRECTORY_SEPARATOR . $dir))
+                        {
+                            //Another directory that needs to be explored
+                            $this->loadFiles($path . DIRECTORY_SEPARATOR . $dir, true);
+                        }
+                        else
+                        {
+                            //List of files
+                            if ($dir != null && count($this->extension_filter) > 0 && in_array($this->getExtension($dir), $this->extension_filter)) {
+                                $path_to_file = $path . DIRECTORY_SEPARATOR . $dir;
+                                $f = new File($dir, $this->getSize($path_to_file), $this->getFullPath() . $path_to_file, $this->getMimeType($path_to_file), 0);
+                                $this->files[] = $f;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    public function getFiles(string $path = ''): array
+    public function getFiles(string $path = '', bool $recursive = false, array $extension_filter = []): array
     {
+        if (count($extension_filter) > 0) {
+            $this->extension_filter = $this->toLowerCase($extension_filter);
+        }
+
         if (count($this->files) == 0 || $path != null) {
-            $this->loadFiles($path);
+            $this->loadFiles($path, $recursive);
         }
+
         return $this->files;
     }
  }
