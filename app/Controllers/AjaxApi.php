dcbec7f01e71c26e3802a354395c4a62a61fe11e
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-08-25 16:04:26 +0100

    Removed HPOQuery and build_tree.

--- app/Controllers/AjaxApi.php
+++ app/Controllers/AjaxApi.php
@@ -103,68 +103,6 @@ use CodeIgniter\Config\Services;
         }
     }
 
-    function HPOQuery(string $hpo_term = ''){
-
-        $hpoNetworkInterface = new HPONetworkInterface();
-        $results = $hpoNetworkInterface->getHPO($hpo_term);
-        return json_encode($results);
-    }
-
-    function build_tree() {
-        if ($this->request->isAJAX())
-        {
-            $hpoNetworkInterface = new HPONetworkInterface();
-
-            $hpo_json = json_decode(stripslashes($this->request->getVar('hpo_json')), 1);
-            $hpo_json = json_decode(str_replace('"true"', 'true', json_encode($hpo_json)), 1);
-            $hpo_json = json_decode(str_replace('"false"', 'false', json_encode($hpo_json)), 1);
-
-            $ancestry = $this->request->getVar('ancestry');
-            $hp_term = explode(' ', $this->request->getVar('hp_term'))[0];
-
-            $splits = explode('||', $ancestry);
-            foreach ($splits as $split) {
-                $parent = &$hpo_json;
-
-                $ancestor = explode('|', $split);
-                $str = 'HP:0000001';
-                foreach(array_reverse($ancestor) as $term) {
-                    if($term === 'HP:0000001') continue;
-
-                    $str .= ".$term";
-                    if(array_key_exists('children', $parent) && is_array($parent['children'])) {
-                        foreach($parent['children'] as &$child) {
-                            if($child['id'] === $str) {
-                                $parent = &$child;
-                                if(array_key_exists('children', $parent) && is_array($child['children'])) {
-                                    $parent['children'] = &$child['children'];
-                                } else {
-                                    $parent['state']['opened'] = "true";
-                                    unset($parent['state']['loading']);
-                                    unset($parent['state']['loaded']);
-                                    $dat = json_encode($hpoNetworkInterface->getHPO($str));
-                                    $parent['children'] = json_decode($dat, 1);
-                                }
-                                break;
-                            }
-                        }
-                    }
-                }
-                if(array_key_exists('children', $parent)) {
-                    foreach($parent['children'] as &$child) {
-                        if($child['id'] === $str . ".$hp_term") {
-                            $child['state']['selected'] = "true";
-                        }
-                    }
-                }
-            }
-            $hpo_json = str_replace('"true"', 'true', $hpo_json);
-            $hpo_json = str_replace('"false"', 'false', $hpo_json);
-
-            return json_encode($hpo_json);
-        }
-	}
-
 	/**
      * getPhenotypeAttributes
      * @param string network_key
