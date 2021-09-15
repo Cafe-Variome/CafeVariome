9d1ca821896236545d3890f2dcd17518c7510f46
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-09-15 15:34:47 +0100

    Created attribute.js file.

---
+++ resources/js/cafevariome/attribute.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // If the table exists, populate DataTable and make it stylish.
+    if ($('#attributestable').length) {
+        $('#attributestable').DataTable();
+    }
+});
