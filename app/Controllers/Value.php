ec30f87f6a97f1a38ea8dc61d7020154e0179f9c
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-09-16 16:18:35 +0100

    Added update function.

--- app/Controllers/Value.php
+++ app/Controllers/Value.php
@@ -69,4 +69,85 @@ class Value extends CVUI_Controller
 
 		return view($this->viewDirectory.'/List', $data);
 	}
+
+	public function Update(int $value_id)
+	{
+		$value = $this->valueModel->getValueById($value_id);
+		if ($value == null || $value_id <= 0){
+			return redirect()->to(base_url('Source'));
+		}
+
+		$uidata = new UIData();
+		$uidata->title = 'Edit Value';
+		$attribute_id = $value['attribute_id'];
+		$uidata->data['attribute_id'] = $attribute_id;
+		$uidata->data['value_id'] = $value['id'];
+
+		$this->validation->setRules([
+			'display_name' => [
+				'label'  => 'Display Name',
+				'rules'  => 'required|string',
+				'errors' => [
+					'required' => '{field} is required.'
+				]
+			]
+		]);
+
+		if ($this->request->getPost() && $this->validation->withRequest($this->request)->run()) {
+			$name = $this->request->getVar('name');
+			$display_name = $this->request->getVar('display_name');
+			$show_in_interface = ($this->request->getVar('show_in_interface') != null) ? true :  false;
+			$include_in_interface_index = ($this->request->getVar('include_in_interface_index') != null) ? true :  false;
+
+			try {
+				$this->valueModel->updateValue($value_id, $display_name, $show_in_interface, $include_in_interface_index);
+				$this->setStatusMessage("Value '$name' was updated.", STATUS_SUCCESS);
+			}
+			catch (\Exception $ex) {
+				$this->setStatusMessage("There was a problem updating '$name'.", STATUS_ERROR);
+			}
+			return redirect()->to(base_url($this->controllerName.'/List/' . $attribute_id));
+
+		}
+		else{
+			$uidata->data['statusMessage'] = $this->validation->getErrors() ? $this->validation->listErrors($this->validationListTemplate) : $this->session->getFlashdata('message');
+
+			$uidata->data['name'] = array(
+				'name' => 'name',
+				'id' => 'name',
+				'type' => 'text',
+				'class' => 'form-control',
+				'readonly' => 'true', // Don't allow the user to edit the attribute name
+				'value' => set_value('name', $value['name']),
+			);
+
+			$uidata->data['display_name'] = array(
+				'name' => 'display_name',
+				'id' => 'display_name',
+				'type' => 'text',
+				'class' => 'form-control',
+				'value' => set_value('name', $value['display_name']),
+			);
+
+			$uidata->data['show_in_interface'] = array(
+				'name' => 'show_in_interface',
+				'id' => 'show_in_interface',
+				'class' => 'custom-control-input',
+				'value' => set_value('show_in_interface', $value['show_in_interface']),
+				'checked' => $value['show_in_interface'] ? true : false
+			);
+
+			$uidata->data['include_in_interface_index'] = array(
+				'name' => 'include_in_interface_index',
+				'id' => 'include_in_interface_index',
+				'class' => 'custom-control-input',
+				'value' => set_value('include_in_interface_index', $value['include_in_interface_index']),
+				'checked' => $value['include_in_interface_index'] ? true : false
+			);
+
+			$data = $this->wrapData($uidata);
+			return view($this->viewDirectory.'/Update', $data);
+		}
+
+	}
 }
---
+++ app/Views/Value/Update.php
@@ -0,0 +1,69 @@
+<?= $this->extend('layout/dashboard') ?>
+<?= $this->section('content') ?>
+
+<div class="row">
+	<div class="col">
+		<h2><?= $title ?></h2>
+	</div>
+</div>
+<hr>
+<?php if($statusMessage): ?>
+	<div class="row">
+		<div class="col">
+			<div class="alert alert-<?= $statusMessageType ?>">
+				<?php echo $statusMessage ?>
+			</div>
+		</div>
+	</div>
+<?php endif; ?>
+<?= form_open($controllerName . '/Update/' . $value_id) ?>
+<div class="form-group row">
+	<div class="col-6">
+		<?php echo form_label('Attribute Name', 'name'); ?>
+		<?php echo form_input($name); ?>
+		<small class="form-text text-muted">
+			You cannot edit value name as it will be used to index the data associated with it.
+		</small>
+	</div>
+	<div class="col-6">
+	</div>
+</div>
+<div class="form-group row">
+	<div class="col-6">
+		<?php echo form_label('Display Name', 'display_name'); ?>
+		<?php echo form_input($display_name); ?>
+		<small class="form-text text-muted">
+			This name will be shown in the query interface.
+		</small>
+	</div>
+	<div class="col-6">
+	</div>
+</div>
+<div class="form-group row">
+	<div class="col">
+		<div class="custom-control custom-checkbox">
+			<?php echo form_checkbox($show_in_interface); ?>
+			<?php echo form_label('Show in Query Interface', 'show_in_interface', array('class'=>'custom-control-label')) ?>
+		</div>
+	</div>
+</div>
+<div class="form-group row">
+	<div class="col">
+		<div class="custom-control custom-checkbox">
+			<?php echo form_checkbox($include_in_interface_index); ?>
+			<?php echo form_label('Include in Query Interface Index', 'include_in_interface_index', array('class'=>'custom-control-label')) ?>
+		</div>
+	</div>
+</div>
+<div class="form-group row">
+	<div class="col">
+		<button type="submit" name="submit" class="btn btn-primary bg-gradient-primary">
+			<i class="fa fa-save"></i>  Save Value
+		</button>
+		<a href="<?php echo base_url($controllerName.'/List/' . $attribute_id); ?>" class="btn btn-secondary bg-gradient-secondary">
+			<i class="fa fa-arrow-left"></i> Go Back to Values List
+		</a>
+	</div>
+</div>
+<?= form_close() ?>
+<?= $this->endSection() ?>
