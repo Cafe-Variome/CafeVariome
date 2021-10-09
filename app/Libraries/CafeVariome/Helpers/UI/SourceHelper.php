f51a9b69bb71aaec001f62497df8bb864ca9d4f3
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-10-09 15:10:23 +0100

    Created SourceHelper class.

---
+++ app/Libraries/CafeVariome/Helpers/UI/SourceHelper.php
@@ -0,0 +1,63 @@
+<?php namespace App\Libraries\CafeVariome\Helpers\UI;
+
+/**
+ * SourceHelper.php
+ * Created 04/10/2021
+ *
+ * This class offers helper functions for sources in the user interface.
+ * @author Mehdi Mehtarizadeh
+ */
+
+class SourceHelper
+{
+	public static function getElasticsearchIndexStatus(int $status)
+	{
+		switch ($status){
+			case ELASTICSEARCH_INDEX_STATUS_UNKNOWN:
+				return 'Unknown';
+			case ELASTICSEARCH_INDEX_STATUS_CREATED:
+				return 'Created';
+			case ELASTICSEARCH_INDEX_STATUS_NOT_CREATED:
+				return 'Not Created';
+		}
+		return 'Undefined';
+	}
+
+	public static function getElasticsearchDataStatus(int $status)
+	{
+		switch ($status){
+			case ELASTICSEARCH_DATA_STATUS_UNKNOWN:
+				return 'Unknown';
+			case ELASTICSEARCH_DATA_STATUS_NOT_INDEXED:
+				return 'Not Indexed';
+			case ELASTICSEARCH_DATA_STATUS_FULLY_INDEXED:
+				return 'Fully Indexed';
+			case ELASTICSEARCH_DATA_STATUS_PARTIALLY_INDEXED:
+				return 'Partially Indexed';
+			case ELASTICSEARCH_DATA_STATUS_EMPTY:
+				return 'Source Empty';
+		}
+		return 'Undefined';
+	}
+
+	public static function formatSize(int $size): string
+	{
+		if ($size < 0){
+			return "Invalid size";
+		}
+		if ($size < 1024){
+			return $size . " Bytes";
+		}
+		else if($size < 1048576){
+			return round($size/1024, 2) . " KB";
+		}
+		else if($size < 1073741824){
+			return round($size/1048576, 2) . " MB";
+		}
+		else {
+			return round($size/1099511627776, 2) . " GB";
+		}
+		return "Invalid size";
+
+	}
+}
