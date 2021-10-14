f99e57422e45b28c72cdf380888a4b3a49190dff
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-10-14 10:52:44 +0100

    Added GetNeo4JStatus function.

--- app/Libraries/CafeVariome/Net/ServiceInterface.php
+++ app/Libraries/CafeVariome/Net/ServiceInterface.php
@@ -60,6 +60,20 @@ class ServiceInterface
         return $results;
     }
 
+	public function GetNeo4JStatus()
+	{
+		$message = ['type' => 'neo4jstatus'];
+
+		$results = "";
+		$this->socket->Create()->Connect()->Write($message);
+		while ($out = $this->socket->Read(2048)) {
+			$results .= $out;
+		}
+		$this->socket->Close();
+
+		return $results;
+	}
+
     public function RegisterProcess(int $entity_id, int $total_records, string $name, string $status = "", string $message = "records_count"): bool
     {
         $message = [
