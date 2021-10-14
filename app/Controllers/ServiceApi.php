6fe1f3321cab1ab45dbcab409134b29616ee1695
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-10-14 10:52:21 +0100

    Added pollNeo4J function.

--- app/Controllers/ServiceApi.php
+++ app/Controllers/ServiceApi.php
@@ -2,9 +2,9 @@
 
 /**
  * ServiceApi.php
- * 
+ *
  * Created : 17/02/2021
- * 
+ *
  * @author Mehdi Mehtarizadeh
 */
 
@@ -23,7 +23,7 @@ class ServiceApi extends ResourceController
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         parent::initController($request, $response, $logger);
- 
+
     }
 
     public function pollUploadedFiles()
@@ -74,9 +74,31 @@ class ServiceApi extends ResourceController
         $this->response->setHeader("Content-Type", "text/event-stream");
         $this->response->setHeader("Cache-Control", "no-cache");
 
-
         return $this->respond($result);
-
     }
+
+	public function pollNeo4J()
+	{
+		$si = new ServiceInterface();
+		$result = "retry: 3000\nid: 0\ndata: {\"progress\": \"-1\", \"status\": \"\"}\n\n";
+		try {
+			$fileStatus = json_decode($si->GetNeo4JStatus(), true);
+
+			if (json_last_error() == JSON_ERROR_NONE && count($fileStatus) > 0) {
+				$result = "retry: 1000\n";
+				foreach ($fileStatus as $taskId => $value) {
+					$result .= "id: " . $taskId . "\n";
+					$result .= "data: {\"progress\": \"" . $value['progress'] . "\", \"status\": \"" . $value['status'] . "\"}\n\n";
+				}
+			}
+		} catch (\Exception $ex) {
+
+		}
+
+		$this->response->setHeader("Content-Type", "text/event-stream");
+		$this->response->setHeader("Cache-Control", "no-cache");
+
+		return $this->respond($result);
+	}
 
 }
