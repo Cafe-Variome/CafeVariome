5c67ce0cfbaae7042a3128acf62d217eb3baeed2
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-10-14 11:31:37 +0100

    Added getOntologyAssociationsByAttributeId function.

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -277,4 +277,14 @@ class Attribute extends Model
 		$this->builder->delete();
 		$this->builder = $this->db->table($this->table);
 	}
+
+	public function getOntologyAssociationsByAttributeId(int $attribute_id): array
+	{
+		$this->builder = $this->db->table('attributes_ontology_prefixes_relationships');
+		$this->builder->where('attribute_id', $attribute_id);
+		$result = $this->builder->get()->getResultArray();
+		$this->builder = $this->db->table($this->table);
+
+		return $result;
+	}
 }
