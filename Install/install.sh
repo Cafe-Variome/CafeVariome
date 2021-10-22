eebca63cbf69f20d3e74237a38d83296eb2322be
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-10-22 01:15:31 +0100

    Added user_interface_index for permission setting.

--- Install/install.sh
+++ Install/install.sh
@@ -12,6 +12,7 @@ setfacl -m u:$apacheUser:rwx writable/cache/
 setfacl -m u:$apacheUser:rwx writable/uploads/
 setfacl -m u:$apacheUser:rwx writable/session/
 setfacl -m u:$apacheUser:rwx resources/phenotype_lookup_data/
+setfacl -m u:$apacheUser:rwx resources/user_interface_index/
 setfacl -m u:$apacheUser:rwx upload/
 setfacl -m u:$apacheUser:rwx upload/pairings/
 setfacl -m u:$apacheUser:rwx upload/UploadData/
