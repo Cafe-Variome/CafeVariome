ba1344826ef5c41136dc4b644c69d828acdf1f83
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-10-25 12:59:19 +0100

    Added getESIndexName.

--- app/Libraries/CafeVariome/Query/AbstractResult.php
+++ app/Libraries/CafeVariome/Query/AbstractResult.php
@@ -1,6 +1,8 @@
 <?php namespace App\Libraries\CafeVariome\Query;
 
 
+use App\Libraries\CafeVariome\Helpers\Core\ElasticsearchHelper;
+
 /**
  * AbstractResult.php
  * Created 23/07/2021
@@ -13,4 +15,9 @@
 abstract class AbstractResult
 {
 	public abstract function extract(array $ids, string $attribute, int $source_id): array;
+
+	protected function getESIndexName(int $source_id): string
+	{
+		return ElasticsearchHelper::getSourceIndexName($source_id);
+	}
 }
