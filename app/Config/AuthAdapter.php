c3278c4ed18410560662134035c774a4ebaa4be4
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-11-03 10:02:27 +0000

    Fixed spacing.

--- app/Config/AuthAdapter.php
+++ app/Config/AuthAdapter.php
@@ -5,9 +5,9 @@ namespace Config;
  * AuthAdapter.php
  * Created 24/07/2019
  * @author Mehdi Mehtarizadeh
- * 
+ *
  * This file contains configuration for AuthAdapter class.
- * 
+ *
  */
 
 class AuthAdapter extends \CodeIgniter\Config\BaseConfig
