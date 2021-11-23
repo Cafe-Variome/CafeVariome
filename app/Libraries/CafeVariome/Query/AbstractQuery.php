80ba91abd196b4be2f75956059d3c1445071d5db
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2021-11-23 16:05:06 +0000

    Added getAttribute and getValue functions.

--- app/Libraries/CafeVariome/Query/AbstractQuery.php
+++ app/Libraries/CafeVariome/Query/AbstractQuery.php
@@ -3,7 +3,11 @@
 
 use App\Libraries\CafeVariome\Core\DataPipeLine\Index\Neo4J;
 use App\Libraries\CafeVariome\Helpers\Core\ElasticsearchHelper;
+use App\Models\Attribute;
+use App\Models\AttributeMapping;
 use App\Models\Settings;
+use App\Models\Value;
+use App\Models\ValueMapping;
 use Elasticsearch\ClientBuilder;
 
 /**
@@ -35,5 +39,33 @@ abstract class AbstractQuery
 	protected function getESIndexName(int $source_id): string
 	{
 		return ElasticsearchHelper::getSourceIndexName($source_id);
+	}
+
+	protected function getAttribute(string $attribute, int $source_id)
+	{
+		// If attribute exists, return it.
+		$attributeModel = new Attribute();
+		$attributeObj = $attributeModel->getAttributeByNameAndSourceId($attribute, $source_id);
+		if ($attributeObj != null) {
+			return $attributeObj;
+		}
+
+		// If attribute mapping exists, return the corresponding attribute.
+		$attributeMappingModel = new AttributeMapping();
+		return $attributeMappingModel->getAttributeByMappingNameAndSourceId($attribute, $source_id);
+	}
+
+	protected function getValue(string $value, int $attribute_id)
+	{
+		//If value exists, return it.
+		$valueModel = new Value();
+		$valueObj = $valueModel->getValueByNameAndAttributeId($value, $attribute_id);
+		if ($valueObj != null){
+			return $valueObj;
+		}
+
+		// If value mapping exists, return the corresponding value.
+		$valueMappingModel = new ValueMapping();
+		return $valueMappingModel->getValueByMappingNameAndAttributeId($value, $attribute_id);
 	}
 }
