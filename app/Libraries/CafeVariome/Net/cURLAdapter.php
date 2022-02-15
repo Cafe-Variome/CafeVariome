88922219341029cc077815506dcea365ab0ba8a6
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-02-15 16:49:18 +0000

    Fixed indentations.

--- app/Libraries/CafeVariome/Net/cURLAdapter.php
+++ app/Libraries/CafeVariome/Net/cURLAdapter.php
@@ -15,27 +15,33 @@
 
     private $baseUrl;
 
-    public function __construct(string $url = null, array $option_values = null) {
-
-        if ($this->curlEnabled()) {
-            if ($url) {
+    public function __construct(string $url = null, array $option_values = null)
+	{
+        if ($this->curlEnabled())
+		{
+            if ($url)
+			{
                 $this->baseUrl = $url;
                 $this->adapterInstance = curl_init($this->baseUrl);
             }
-            else {
+            else
+			{
                 $this->adapterInstance = curl_init();
             }
 
-            if ($option_values && count($option_values) > 0) {
-                foreach ($option_values as $option => $value) {
+            if ($option_values && count($option_values) > 0)
+			{
+                foreach ($option_values as $option => $value)
+				{
                     $this->setOption($option, $value);
                 }
             }
         }
-        else {
+        else
+		{
             throw new \Exception('cURL is not installed or enabled.');
         }
-        
+
     }
 
     public function setOption(int $option, $value): bool
@@ -57,9 +63,9 @@
     {
         return function_exists('curl_version');
     }
-    
-    function __destruct() {
+
+    function __destruct()
+	{
         curl_close($this->adapterInstance);
     }
  }
- 
