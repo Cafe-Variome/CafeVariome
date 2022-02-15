f80f9bbb0b5dd2e16ddbefb9c92dc8c5533154f0
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-02-15 16:52:17 +0000

    Deleted $binPath as CVService is within CV now.

--- app/Config/BackgroundService.php
+++ app/Config/BackgroundService.php
@@ -5,14 +5,13 @@ namespace Config;
  * BackgroundService.php
  * Created 18/02/2021
  * @author Mehdi Mehtarizadeh
- * 
- * This file contains configuration 
- * 
+ *
+ * This file contains configuration
+ *
  */
 
 class BackgroundService extends \CodeIgniter\Config\BaseConfig
 {
     public $address = '127.0.0.1';
     public $port = 49200;
-    public $binPath = '/var/www/ws/cvdaemon.php';
 }
