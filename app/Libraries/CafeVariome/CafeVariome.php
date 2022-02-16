d56d57b7ad6274430547d49b96cadb00f46114b6
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-02-16 11:30:09 +0000

    Added Boot function.

--- app/Libraries/CafeVariome/CafeVariome.php
+++ app/Libraries/CafeVariome/CafeVariome.php
@@ -1,6 +1,33 @@
 <?php namespace App\Libraries\CafeVariome;
 
+/**
+ * CafeVariome.php
+ *
+ * @auhtor Mehdi Mehtarizadeh
+ *
+ */
 
-class CafeVariome{
-    
+class CafeVariome
+{
+
+	/**
+	 * Boot - Initialises constants from .env file
+	 * @return void
+	 */
+	public static function Boot()
+	{
+		// PHP Binary Path, used in CLI calls
+		define('PHP_BIN_PATH', getenv('PHP_BIN_PATH'));
+
+		//Data stream batch sizes
+		define('EAV_BATCH_SIZE', getenv('EAV_BATCH_SIZE'));
+		define('NEO4J_BATCH_SIZE', getenv('NEO4J_BATCH_SIZE'));
+
+		//Data input batch size used in transactions
+		define('SPREADSHEET_BATCH_SIZE', getenv('SPREADSHEET_BATCH_SIZE'));
+
+		//Elasticsearch Aggregate Size
+		define('ELASTICSERACH_AGGREGATE_SIZE', getenv('ELASTICSERACH_AGGREGATE_SIZE'));
+		define('ELASTICSERACH_EXTRACT_AGGREGATE_SIZE', getenv('ELASTICSERACH_EXTRACT_AGGREGATE_SIZE'));
+	}
 }
