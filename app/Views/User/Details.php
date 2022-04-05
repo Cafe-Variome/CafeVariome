bb6e9b19303285053420eed007b2ac9abf1db514
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-04-05 13:55:36 +0100

    Fixed user details table.

--- app/Views/User/Details.php
+++ app/Views/User/Details.php
@@ -10,69 +10,79 @@
 <div class="row">
     <div class="col">
         <table class="table table-bordered table-striped table-hover" id="userdetailstable" style="width:40%;">
-
             <tr>
                 <th>ID</th>
                 <td><?php echo $id; ?></td>
             </tr>
-            <th>Full Name</th>
-            <td><?php echo $first_name . " " . $last_name; ?></td>
-
+			<tr>
+				<th>Full Name</th>
+				<td><?php echo $first_name . " " . $last_name; ?></td>
             </tr>
-            <th>Email</th>
-            <td><?php echo $uemail; ?></td>
+			<tr>
+				<th>Email</th>
+				<td><?php echo $uemail; ?></td>
             </tr>
-            <th>Company</th>
-            <td><?php echo $company; ?></td>
+			<tr>
+				<th>Company</th>
+				<td><?php echo $company; ?></td>
             </tr>
-            <th>Phone</th>
-            <td><?php echo $phone; ?></td>
+			<tr>
+				<th>Phone</th>
+				<td><?php echo $phone; ?></td>
             </tr>
+			<tr>
             <th>Remote</th>
-            <td><?php if ($remote) {
-                    echo 'Remote User';
-                } else {
-                    echo 'Local User';
-                } ?></td>
+				<td><?php if ($remote) {
+						echo 'Remote User';
+					} else {
+						echo 'Local User';
+					} ?>
+				</td>
             </tr>
+			<tr>
             <th>Network Groups</th>
-            <td>
-                <?php if (isset($users_groups)) : ?>
-                    <?php if (array_key_exists($id, $users_groups)) : ?>
-                        <?php foreach ($users_groups[$id] as $group) : ?>
-                            <?php echo "<i>'" . $group['group_description'] . "' Network Group</i> (Network: " . $group['network_name'] . ");<br/>" ?>
-                        <?php endforeach ?>
-                    <?php else : echo "None"; ?>
-                    <?php endif; ?>
-                <?php endif; ?>
-            </td>
+				<td>
+					<?php if (isset($users_groups)) : ?>
+						<?php if (array_key_exists($id, $users_groups)) : ?>
+							<?php foreach ($users_groups[$id] as $group) : ?>
+								<?php echo "<i>'" . $group['group_description'] . "' Network Group</i> (Network: " . $group['network_name'] . ");<br/>" ?>
+							<?php endforeach ?>
+						<?php else : echo "None"; ?>
+						<?php endif; ?>
+					<?php endif; ?>
+				</td>
             </tr>
-            <th>Last Login</th>
-            <td><?= $last_login; ?></td>
+			<tr>
+				<th>Last Login</th>
+				<td><?= $last_login; ?></td>
             </tr>
-            <th>Created On</th>
-            <td><?= $created_on; ?></td>
+			<tr>
+				<th>Created On</th>
+				<td><?= $created_on; ?></td>
             </tr>
-            <th>Role</th>
-            <td>
-                <?php if ($is_admin) {
-                    echo 'Admin';
-                } else {
-                    echo 'User';
-                } ?>
-            </td>
+			<tr>
+				<th>Role</th>
+				<td>
+					<?php if ($is_admin) {
+						echo 'Admin';
+					} else {
+						echo 'User';
+					} ?>
+				</td>
             </tr>
-            <th>IP Address</th>
-            <td><?php echo $ip_address; ?></td>
+			<tr>
+				<th>IP Address</th>
+				<td><?php echo $ip_address; ?></td>
             </tr>
-            <th>Status</th>
-            <td><?php if ($active) {
-                    echo 'Active';
-                } else {
-                    echo 'Inactive';
-                } ?></td>
+			<tr>
+            	<th>Status</th>
+				<td><?php if ($active) {
+						echo 'Active';
+					} else {
+						echo 'Inactive';
+					} ?>
+				</td>
             </tr>
-
         </table>
         <hr />
         <a href="<?= base_url($controllerName . '/Update') . "/" . $id; ?>" class="btn btn-warning bg-gradient-warning">
