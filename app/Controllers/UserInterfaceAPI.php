b39e3e8da7d43ebba91ede6de4454120441a2850
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-04-05 13:25:32 +0100

    Created UserInterfaceAPI.

---
+++ app/Controllers/UserInterfaceAPI.php
@@ -0,0 +1,48 @@
+<?php namespace App\Controllers;
+
+use App\Models\Settings;
+
+/**
+ * UserInterfaceAPI.php
+ *
+ * Created 05/04/2022
+ *
+ * @author Mehdi Mehtraizadeh
+ *
+ * This controller contains endpoints that generate UI JavaScript code.
+ */
+
+class UserInterfaceAPI extends BaseController
+{
+
+	private $setting;
+
+	public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
+	{
+		parent::initController($request, $response, $logger);
+		$this->setting =  Settings::getInstance();
+	}
+
+	public function getUIConstants()
+	{
+		$this->response->setHeader("Content-Type", "text/javascript");
+
+		$script = '';
+
+		$baseURL = base_url(); // Load base URL
+		$hpoAutocompleteURL = $this->setting->getHPOAutoCompleteURL();
+		$orphaAutocompleteURL = $this->setting->getORPHAAutoCompleteURL();
+		$snomedAutocompleteURL = $this->setting->getSNOMEDAutoCompleteURL();
+		$geneAutocompleteURL = $this->setting->getGeneAutoCompleteURL();
+
+		$script .= "var baseurl = '$baseURL/';";
+		$script .= "var hpo_autocomplete_url = '$hpoAutocompleteURL';";
+		$script .= "var orpha_autocomplete_url = '$orphaAutocompleteURL';";
+		$script .= "var snomed_autocomplete_url = '$snomedAutocompleteURL';";
+		$script .= "var gene_autocomplete_url = '$geneAutocompleteURL';";
+
+		return $script;
+
+	}
+
+}
