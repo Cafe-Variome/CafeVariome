f6eb75847f4c7cd39f500b2e2a69e6f36c7440fa
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-04-27 15:41:58 +0100

    Created ServerAdapterFactory class.

---
+++ app/Libraries/CafeVariome/Factory/ServerAdapterFactory.php
@@ -0,0 +1,24 @@
+<?php namespace App\Libraries\CafeVariome\Factory;
+
+/**
+ * ServerAdapterFactory.php
+ * Created 25/04/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Database\IAdapter;
+use App\Libraries\CafeVariome\Database\ServerAdapter;
+
+class ServerAdapterFactory extends AdapterFactory
+{
+
+	/**
+	 * Creates and returns an object of the ServerAdapter type.
+	 * @return IAdapter
+	 */
+    public function getInstance(): IAdapter
+    {
+        return new ServerAdapter();
+    }
+}
