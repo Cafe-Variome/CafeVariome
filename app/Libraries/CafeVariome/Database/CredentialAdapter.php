c5231d307803c9abd59c21efd855417ee7a2bb54
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-05-12 14:37:35 +0100

    Added CredentialAdapter class.

---
+++ app/Libraries/CafeVariome/Database/CredentialAdapter.php
@@ -0,0 +1,37 @@
+<?php namespace App\Libraries\CafeVariome\Database;
+
+/**
+ * CredentialAdapter.php
+ * Created 22/04/2022
+ *
+ * This class offers CRUD operation for Credential.
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Entities\IEntity;
+use App\Libraries\CafeVariome\Factory\CredentialFactory;
+
+class CredentialAdapter extends BaseAdapter
+{
+	/**
+	 * @inheritDoc
+	 */
+	protected string $table = 'credentials';
+
+	/**
+	 * @inheritDoc
+	 */
+	protected string $key = 'id';
+
+	/**
+	 * Converts general PHP objects to a Credential object.
+	 * @param object|null $object
+	 * @return IEntity
+	 * @throws \Exception
+	 */
+    public function toEntity(?object $object): IEntity
+    {
+        $credentialFactory = new CredentialFactory();
+		return $credentialFactory->getInstance($object);
+    }
+}
