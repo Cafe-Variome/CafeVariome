1a41225214c3ccbf098719ffe7972aecb6bcf84d
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-05-12 14:37:43 +0100

    Added CredentialAdapterFactory class.

---
+++ app/Libraries/CafeVariome/Factory/CredentialAdapterFactory.php
@@ -0,0 +1,23 @@
+<?php namespace App\Libraries\CafeVariome\Factory;
+
+/**
+ * CredentialAdapterFactory.php
+ * Created 03/05/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Database\CredentialAdapter;
+use App\Libraries\CafeVariome\Database\IAdapter;
+
+class CredentialAdapterFactory extends AdapterFactory
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getInstance(): IAdapter
+    {
+        return new CredentialAdapter();
+    }
+}
