6b35df0e4523e93ade3fd28e846ad72f2191de2f
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-05-30 16:39:00 +0100

    Created UserAdapterFactory class.

---
+++ app/Libraries/CafeVariome/Factory/UserAdapterFactory.php
@@ -0,0 +1,23 @@
+<?php namespace App\Libraries\CafeVariome\Factory;
+
+/**
+ * UserAdapterFactory.php
+ * Created 27/05/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Database\IAdapter;
+use App\Libraries\CafeVariome\Database\UserAdapter;
+
+class UserAdapterFactory extends AdapterFactory
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getInstance(): IAdapter
+    {
+        return new UserAdapter();
+    }
+}
