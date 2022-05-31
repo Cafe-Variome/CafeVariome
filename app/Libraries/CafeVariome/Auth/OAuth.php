91b2598d4fb84a6bd4fdc8a0fc0fccc79aa0d159
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-05-31 13:31:46 +0100

    Marked class as deprecated.

--- app/Libraries/CafeVariome/Auth/OAuth.php
+++ app/Libraries/CafeVariome/Auth/OAuth.php
@@ -5,7 +5,7 @@ namespace App\Libraries\CafeVariome\Auth;
  * Name: OAuth.php
  * Created: 02/10/2020
  * @author Mehdi Mehtarizadeh
- * 
+ * @deprecated
  */
 use App\Models\Settings;
 use App\Models\User;
@@ -14,7 +14,8 @@ use App\Models\Network;
 use App\Libraries\CafeVariome\Email\EmailFactory;
 use App\Libraries\CafeVariome\Net\cURLAdapter;
 
-class OAuth extends OpenIDAuthenticator{
+class OAuth extends OpenIDAuthenticator
+{
 
     public function __construct(){
         parent::__construct();
@@ -33,7 +34,8 @@ class OAuth extends OpenIDAuthenticator{
             $this->configureProxy();
         }
 
-        try {
+        try
+		{
             $this->provider = new \League\OAuth2\Client\Provider\GenericProvider($this->options);
         }
         catch (Exception $e) {
