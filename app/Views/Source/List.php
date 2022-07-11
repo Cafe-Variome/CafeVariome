3b705f35e47aa82b77595088493a7e98602043ee
Vatsalya Maddi <vatsalyarini@gmail.com>
2022-07-11 11:50:51 +0100

    updated comments for modals

--- app/Views/Source/List.php
+++ app/Views/Source/List.php
@@ -20,7 +20,6 @@
 	<thead>
 		<tr>
 			<th>Name</th>
-			<!-- <th>Description</th> -->
 			<th>Record Count</th>
 			<th>Assigned Group(s)</th>
 			<th>Status</th>
@@ -30,7 +29,6 @@
 	<tbody>
 		<?php foreach ($sources as $source) : ?>
 			<td><?php echo $source['name']; ?></td>
-			<!-- <td><?php echo $source['description']; ?></td> -->
 			<td><?= $source['record_count']; ?></td>
 			<td>
 				<?php
@@ -81,6 +79,7 @@
 </div>
 
 <br />
+<!-- FILE MANAGER MODAL -->
 <div id="uploadModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="uploadModalLabel" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
@@ -104,18 +103,6 @@
 						</td>
 					</tr>
 				</table>
-				<!-- <div class="row mb-2">
-					<div class="col">
-						<a class="btn btn-success text-white font-weight-bold" data-toggle="modal" data-target="#addVariantsModal" data-id="<?= $source['source_id'] ?>" data-srcname="<?= $source['name']; ?>" id="ImportRecordsBtn"> <i class="fa fa-upload text-light"></i> Upload Files
-						</a>
-					</div>
-				</div>
-				<div class="row mb-2">
-					<div class="col">
-						<a class="btn btn-primary text-white font-weight-bold" href="<?php echo base_url('Upload/Import') . "/" . $source['source_id']; ?>"> <i class="fa fa-file-import text-light"></i> Import Files
-						</a>
-					</div>
-				</div> -->
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -123,6 +110,8 @@
 		</div>
 	</div>
 </div>
+
+<!-- UPLOAD FILES MODAL -->
 <div id="addVariantsModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="addVariantsModalLabel" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
@@ -152,27 +141,6 @@
 						</td>
 					</tr>
 				</table>
-				<!-- <div class="row mb-2">
-					<div class="col">
-						<a id="bulkImport" class="btn btn-small btn-primary text-white font-weight-bold">
-							<i class="fa fa-file-upload"></i> Upload Spreadsheet Files
-						</a>
-					</div>
-				</div>
-				<div class="row mb-2">
-					<div class="col">
-						<a id="phenoPacketsImport" class="btn btn-small btn-primary text-white font-weight-bold">
-							<i class="fa fa-file-upload"></i> Upload PhenoPacket Files
-						</a>
-					</div>
-				</div>
-				<div class="row mb-2">
-					<div class="col">
-						<a id="VCFImport" class="btn btn-small btn-primary text-white font-weight-bold">
-							<i class="fa fa-file-upload"></i> Upload VCF Files
-						</a>
-					</div>
-				</div> -->
 				<!-- <div class="row">
 					<div class="col">
 						<a id="UniversalImport" class="btn btn-small btn-primary">
@@ -195,8 +163,7 @@
 	</div>
 </div>
 
-
-
+<!-- INDICES MODAL -->
 <div id="indicesModal" class="modal fade" style="justify-content: center;align-items:center" tabindex="-1" role="dialog" aria-labelledby="indicesModalLabel" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
@@ -223,24 +190,6 @@
 						</td>
 					</tr>
 				</table>
-				<!-- <div class="row mb-2">
-					<div class="col">
-						<a class="btn btn-info" href="<?php echo base_url($controllerName . '/Elasticsearch') . "/" . $source['source_id']; ?>" data-toggle="tooltip" data-placement="top" title="Elasticsearch Index"><i class="fa fa-search text-light"></i>ES Index
-						</a>
-					</div>
-				</div>
-				<div class="row mb-2">
-					<div class="col">
-						<a class="btn btn-warning" href="<?php echo base_url($controllerName . '/Neo4J') . "/" . $source['source_id']; ?>" data-toggle="tooltip" data-placement="top" title="Neo4J Index"> <i class="fa fa-project-diagram text-light"></i> Neo4J Index
-						</a>
-					</div>
-				</div>
-				<div class="row mb-2">
-					<div class="col">
-						<a class="btn btn-secondary" href="<?php echo base_url($controllerName . '/UserInterface') . "/" . $source['source_id']; ?>" data-toggle="tooltip" data-placement="top" title="User Interface Index"> <i class="fa fa-desktop text-light"></i> UI Index
-						</a>
-					</div>
-				</div> -->
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -249,6 +198,7 @@
 	</div>
 </div>
 
+<!-- SOURCE ACTIONS/EDITOR MODAL -->
 <div id="sourcesModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="sourcesModalLabel" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
@@ -278,60 +228,6 @@
 		</div>
 	</div>
 </div>
-
-<!-- <?php
-	// $hidden = array('source_id' => 2);
-	// echo form_open($controllerName . '/List', '', $hidden);
-?> -->
-<!-- STATUS MODAL -->
-<!-- <div id="statusModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="statusModalLabel" aria-hidden="true">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h4 class="modal-title" align="center" id="statusModalTitle">Toggle Source for Discovery</h4>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-			<div class="modal-body">
-				<div class="row mb-2">
-					<div class="col">
-						<div class="row" id="statusToggle">
-							<div class="col-4">Set source status:</div>
-							<div class="col-8">
-								<span>
-									<label for="staton">Online</label>
-									<input type="radio" name="statusToggle" id="staton" value="On">
-								</span>
-								<span>
-									<label for="statoff">Offline</label>
-									<input type="radio" name="statusToggle" id="statoff" value="Off">
-								</span>
-							</div>
-						</div>
-						<div>
-							<?php echo form_label('Make this source discoverable in the query interface?', 'status'); ?>
-							<?php
-							$options = array(
-								'online' => 'Yes',
-								'offline' => 'No',
-							);
-							echo form_dropdown('status', $options, 'mysql', ['class' => 'form-control']);
-							?>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="modal-footer">
-				<button type="submit" class="btn btn-primary bg-gradient-primary">
-					<i class="fa fa-save"></i> Save
-				</button>
-				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-			</div>
-		</div>
-	</div>
-</div> -->
-
 <style>
 	.modal-dialog {
 		height: 100vh !important;
