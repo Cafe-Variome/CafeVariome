8332c5325092d8a4f4566db9793b7a2f94dc6848
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-07-28 11:52:12 +0100

    Created PipelineAdapter class.

---
+++ app/Libraries/CafeVariome/Database/PipelineAdapter.php
@@ -0,0 +1,34 @@
+<?php namespace App\Libraries\CafeVariome\Database;
+
+/**
+ * PipelineAdapter.php
+ * Created 30/05/2022
+ *
+ * This class offers CRUD operation for Pipeline.
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Entities\IEntity;
+use App\Libraries\CafeVariome\Factory\PipelineFactory;
+
+class PipelineAdapter extends BaseAdapter
+{
+	/**
+	 * @inheritDoc
+	 */
+	protected string $table = 'pipelines';
+
+	/**
+	 * @inheritDoc
+	 */
+	protected string $key = 'id';
+
+    /**
+     * @inheritDoc
+     */
+    public function toEntity(?object $object): IEntity
+    {
+        $pipelineFactory = new PipelineFactory();
+		return $pipelineFactory->GetInstance($object);
+    }
+}
