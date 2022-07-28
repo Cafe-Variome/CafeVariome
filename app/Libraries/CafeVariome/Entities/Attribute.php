788a5441433fd5c7ded5d9aa828df6fd569350cd
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-07-28 11:48:28 +0100

    Created Attribute entity class.

---
+++ app/Libraries/CafeVariome/Entities/Attribute.php
@@ -0,0 +1,36 @@
+<?php namespace App\Libraries\CafeVariome\Entities;
+
+/**
+ * Attribute.php
+ * Created 28/07/2022
+ *
+ * This class extends Entity.
+ * @author Mehdi Mehtarizadeh
+ */
+
+class Attribute extends Entity
+{
+	public string $name;
+
+	public string $display_name;
+
+	public int $source_id;
+
+	public int $type;
+
+	public float $min;
+
+	public float $max;
+
+	public bool $show_in_interface;
+
+	public bool $include_in_interface_index;
+
+	public int $storage_location;
+
+	public function __construct(array $properties)
+	{
+		parent::__construct($properties);
+	}
+
+}
