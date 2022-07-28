11875ddec30f3a9f0c86f082a9f5edc64b3d0caf
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-07-28 14:29:12 +0100

    Created SourceAdapterFactory class.

---
+++ app/Libraries/CafeVariome/Factory/SourceAdapterFactory.php
@@ -0,0 +1,20 @@
+<?php namespace App\Libraries\CafeVariome\Factory;
+
+/**
+ * SourceAdapterFactory.php
+ * Created 21/06/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Database\IAdapter;
+use App\Libraries\CafeVariome\Database\SourceAdapter;
+
+class SourceAdapterFactory extends AdapterFactory
+{
+
+	public function GetInstance(): IAdapter
+	{
+		return new SourceAdapter();
+	}
+}
