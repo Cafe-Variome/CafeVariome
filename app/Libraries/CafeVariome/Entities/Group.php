998c2d0d31b8faca23a18ff61bf42b79bed52af3
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-07-28 11:49:38 +0100

    Created Group entity class.

---
+++ app/Libraries/CafeVariome/Entities/Group.php
@@ -0,0 +1,21 @@
+<?php namespace App\Libraries\CafeVariome\Entities;
+
+/**
+ * Group.php
+ * Created 27/07/2022
+ *
+ * This class extends Entity and implements IEntity.
+ * @author Mehdi Mehtarizadeh
+ */
+
+class Group extends Entity
+{
+	public string $name;
+
+	public int $source_id;
+
+	public function __construct(array $properties)
+	{
+		parent::__construct($properties);
+	}
+}
