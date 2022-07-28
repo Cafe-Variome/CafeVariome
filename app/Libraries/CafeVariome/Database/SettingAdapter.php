f9a46814046fae9198168816034288d3200a7e95
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-07-28 11:52:27 +0100

    Created SettingAdapter class.

---
+++ app/Libraries/CafeVariome/Database/SettingAdapter.php
@@ -0,0 +1,123 @@
+<?php namespace App\Libraries\CafeVariome\Database;
+
+use App\Libraries\CafeVariome\Entities\IEntity;
+use App\Libraries\CafeVariome\Factory\SettingFactory;
+
+/**
+ * SettingAdapter.php
+ * Created 21/07/2022
+ *
+ * This class offers CRUD operation for Setting.
+ * @author Mehdi Mehtarizadeh
+ */
+
+class SettingAdapter extends BaseAdapter
+{
+	/**
+	 * @inheritDoc
+	 */
+	protected string $table = 'settings';
+
+	/**
+	 * @inheritDoc
+	 */
+	protected string $key = 'key';
+
+	private static ?SettingAdapter $singletonInstance = null;
+
+	private array $settings;
+
+	private function __construct()
+	{
+		parent::__construct();
+		$this->settings = parent::ReadAll();
+
+	}
+
+	public static function GetSingletonInstance(): self
+	{
+		if (self::$singletonInstance == null)
+		{
+			self::$singletonInstance = new SettingAdapter();
+		}
+
+		return self::$singletonInstance;
+	}
+
+	public function GetInstallationKey(): ?string
+	{
+		return $this->settings['installation_key']->value;
+	}
+
+	public function GetAuthServerUrl(): ?string
+	{
+		return $this->settings['auth_server']->value;
+	}
+
+	public function GetElasticSearchUri(): ?string
+	{
+		return $this->settings['elastic_url']->value;
+	}
+
+	public function GetNeo4JUserName(): ?string
+	{
+		return $this->settings['neo4j_username']->value;
+	}
+
+	public function GetNeo4JPassword(): ?string
+	{
+		return $this->settings['neo4j_password']->value;
+	}
+
+	public function GetNeo4JUri(): ?string
+	{
+		return $this->settings['neo4j_server']->value;
+	}
+
+	public function GetNeo4JPort(): ?string
+	{
+		return $this->settings['neo4j_port']->value;
+	}
+
+	public function GetSiteTitle(): ?string
+	{
+		return $this->settings['site_title']->value;
+	}
+
+	public function GetHPOAutoCompleteURL(): ?string
+	{
+		return $this->settings['hpo_autocomplete_url']->value;
+	}
+
+	public function GetORPHAAutoCompleteURL(): ?string
+	{
+		return $this->settings['orpha_autocomplete_url']->value;
+	}
+
+	public function GetSNOMEDAutoCompleteURL(): ?string
+	{
+		return $this->settings['snomed_autocomplete_url']->value;
+	}
+
+	public function GetGeneAutoCompleteURL(): ?string
+	{
+		return $this->settings['gene_autocomplete_url']->value;
+	}
+
+	public function GetHeaderImage(): ?string
+	{
+		return $this->settings['logo']->value;
+	}
+
+	/**
+	 * Converts general PHP objects to a Setting object.
+	 * @param object|null $object
+	 * @return IEntity
+	 * @throws \Exception
+	 */
+	public function toEntity(?object $object): IEntity
+	{
+		$settingFactory = new SettingFactory();
+		return $settingFactory->GetInstance($object);
+	}
+}
