9b486a2c601827b66c07ed01743eb93d8f652327
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-07-29 16:39:58 +0100

    Created AttributeAdapterFactory class.

---
+++ app/Libraries/CafeVariome/Factory/AttributeAdapterFactory.php
@@ -0,0 +1,22 @@
+<?php namespace App\Libraries\CafeVariome\Factory;
+
+/**
+ * AttributeAdapterFactory.php
+ * Created 28/07/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Database\AttributeAdapter;
+use App\Libraries\CafeVariome\Database\IAdapter;
+
+class AttributeAdapterFactory extends AdapterFactory
+{
+    /**
+     * @inheritDoc
+     */
+    public function GetInstance(): IAdapter
+    {
+        return new AttributeAdapter();
+    }
+}
