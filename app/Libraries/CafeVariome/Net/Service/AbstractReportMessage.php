557adbc1e8be2e353589dab924d85f525a1cacdf
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-07-29 16:39:31 +0100

    Created AbstractReportMessage class.

---
+++ app/Libraries/CafeVariome/Net/Service/AbstractReportMessage.php
@@ -0,0 +1,31 @@
+<?php namespace App\Libraries\CafeVariome\Net\Service;
+
+/**
+ * AbstractReportMessage.php
+ * Created 25/07/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+abstract class AbstractReportMessage extends AbstractMessage
+{
+	protected int $process_id;
+
+	protected int $task_id;
+
+	protected bool $finished;
+
+	protected string $type;
+
+	protected string $status;
+
+	public function __construct(int $task_id, bool $finished = false, string $status = '')
+	{
+		parent::__construct();
+		$this->process_id = getmypid();
+		$this->SetType();
+		$this->task_id = $task_id;
+		$this->finished = $finished;
+		$this->status = $status;
+	}
+}
