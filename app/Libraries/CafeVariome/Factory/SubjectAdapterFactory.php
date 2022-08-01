da29a274076324a74261b3482886d8e536dcff52
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-01 11:45:55 +0100

    Created SubjectAdapterFactory class.

---
+++ app/Libraries/CafeVariome/Factory/SubjectAdapterFactory.php
@@ -0,0 +1,22 @@
+<?php namespace App\Libraries\CafeVariome\Factory;
+
+/**
+ * SubjectAdapterFactory.php
+ * Created 27/07/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Database\IAdapter;
+use App\Libraries\CafeVariome\Database\SubjectAdapter;
+
+class SubjectAdapterFactory extends AdapterFactory
+{
+    /**
+     * @inheritDoc
+     */
+    public function GetInstance(): IAdapter
+    {
+        return new SubjectAdapter();
+    }
+}
