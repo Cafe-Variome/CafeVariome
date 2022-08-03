c75031422db92591bbf4a4024cb22f42ae551fd3
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-03 12:18:03 +0100

    Created RegisterTaskMessageFactory class.

---
+++ app/Libraries/CafeVariome/Factory/RegisterTaskMessageFactory.php
@@ -0,0 +1,20 @@
+<?php namespace App\Libraries\CafeVariome\Factory;
+
+/**
+ * RegisterTaskMessageFactory.php
+ * Created 20/07/2022
+ *
+ * This is a factory class for handling object creation of RegisterTaskMessage class.
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Net\Service\RegisterTaskMessage;
+
+class RegisterTaskMessageFactory
+{
+
+    public function GetInstance(int $task_id, string $status = ''): RegisterTaskMessage
+    {
+        return new RegisterTaskMessage($task_id, $status);
+    }
+}
