fe25ad87a4c603a2b3d4eefab5fa7bc5038d2db3
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-03 12:21:43 +0100

    Moved deleteExistingRecords to DataPipeLine.

--- app/Libraries/CafeVariome/Core/DataPipeLine/Input/DataInput.php
+++ app/Libraries/CafeVariome/Core/DataPipeLine/Input/DataInput.php
@@ -259,26 +259,6 @@ abstract class DataInput
 		$db->transComplete();
 	}
 
-	protected function deleteExistingRecords(int $file_id)
-	{
-		$db = \Config\Database::connect();
-		$db->transStart();
-		$valueFrequencies = $this->eavModel->getValueFrequenciesBySourceIdAndFileId($this->sourceId, $file_id); // Get value frequencies
-		if (count($valueFrequencies) > 0){
-			$this->eavModel->deleteRecordsByFileId($file_id); // Delete records from eavs
-			$valueCount = count($valueFrequencies);
-			for ($i = 0; $i < $valueCount; $i++){
-				$value_id = $valueFrequencies[$i]['value_id'];
-				$value_frequency = $valueFrequencies[$i]['frequency'];
-				$this->valueModel->updateFrequency($value_id, -$value_frequency);
-				$this->valueModel->deleteAbsentValue($value_id); // Delete value if frequency is 0
-				unset($valueFrequencies[$i]);
-			}
-			unset($value_id);
-		}
-		$db->transComplete();
-	}
-
 	protected function determineAttributesType()
 	{
 		$db = \Config\Database::connect();
