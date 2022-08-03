23e7ca2b826cc46a5d1230eb42d193fd1a5b2ade
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-03 12:17:32 +0100

    Created IMessage interface.

---
+++ app/Libraries/CafeVariome/Net/Service/IMessage.php
@@ -0,0 +1,14 @@
+<?php namespace App\Libraries\CafeVariome\Net\Service;
+
+/**
+ * IMessage.php
+ * Created 18/07/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+interface IMessage
+{
+
+	public function ToJson(): string;
+}
