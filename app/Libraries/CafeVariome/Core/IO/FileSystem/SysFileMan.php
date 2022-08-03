49148bc27b0eabb0b5321cdcfc38259f92101380
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-03 12:14:54 +0100

    Added support for diskName usage.

--- app/Libraries/CafeVariome/Core/IO/FileSystem/SysFileMan.php
+++ app/Libraries/CafeVariome/Core/IO/FileSystem/SysFileMan.php
@@ -15,10 +15,11 @@
     protected array $files;
     private array $extension_filter;
 
-    public function __construct(string $basePath = null, bool $recursive = false, array $extension_filter = [])
+    public function __construct(string $basePath = null, bool $recursive = false, array $extension_filter = [], bool $useDiskName = false, $diskNameLength = 16)
 	{
         parent::__construct($basePath);
-
+		$this->useDiskName = $useDiskName;
+		$this->diskNameLength = $diskNameLength;
         $this->files = [];
         $this->extension_filter = $this->toLowerCase($extension_filter);
         $this->loadFiles('', $recursive);
@@ -35,17 +36,37 @@
 
     private function loadFiles(string $path = '', bool $recursive = false)
     {
-        if(!$recursive){
-            if ($this->Exists($path)) {
-                if (is_dir($this->getFullPath() . $path)) {
-                    $this->files = scandir($this->getFullPath() . $path);
+        if(!$recursive)
+		{
+            if ($this->Exists($path))
+			{
+                if (is_dir($this->getFullPath() . $path))
+				{
+					$dir_content = @scandir($this->getFullPath() . $path);
+					for($c = 0; $c < count($dir_content); $c++)
+					{
+						if ($dir_content[$c] == '.' || $dir_content[$c] == '..') continue;
+
+						$path_to_file = $path . DIRECTORY_SEPARATOR;
+						$this->files[] = new File(
+							$dir_content[$c],
+							$this->getSize($path_to_file),
+							$this->getFullPath() . $path_to_file,
+							$this->getMimeType($path_to_file),
+							0,
+							$this->useDiskName,
+							$this->diskNameLength
+						);
+					}
+
                 }
             }
         }
-        else{
+        else
+		{
             $cdir = @scandir($this->getFullPath() . $path);
-            if (is_array($cdir)) {
-
+            if (is_array($cdir))
+			{
                 foreach ($cdir as $dir)
                 {
                     if (!in_array($dir, [".",".."]))
@@ -58,9 +79,18 @@
                         else
                         {
                             //List of files
-                            if ($dir != null && count($this->extension_filter) > 0 && in_array($this->getExtension($dir), $this->extension_filter)) {
+                            if ($dir != null && count($this->extension_filter) > 0 && in_array($this->getExtension($dir), $this->extension_filter))
+							{
                                 $path_to_file = $path . DIRECTORY_SEPARATOR . $dir;
-                                $f = new File($dir, $this->getSize($path_to_file), $this->getFullPath() . $path_to_file, $this->getMimeType($path_to_file), 0);
+                                $f = new File(
+									$dir,
+									$this->getSize($path_to_file),
+									$this->getFullPath() . $path_to_file,
+									$this->getMimeType($path_to_file),
+									0,
+									$this->useDiskName,
+									$this->diskNameLength
+								);
                                 $this->files[] = $f;
                             }
                         }
@@ -85,6 +115,8 @@
 
     public function Save(File $file, string $path = '') : bool
     {
-        return copy($file->getTempPath(), $this->getFullPath() . $path . basename($file->getName()));
+		$file_name_to_write = $this->useDiskName ? $file->getDiskName() : basename($file->getName());
+
+		return copy($file->getTempPath(), $this->getFullPath() . $path . $file_name_to_write);
     }
  }
