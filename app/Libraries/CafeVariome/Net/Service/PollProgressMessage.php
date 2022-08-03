a9300a27f0349264832051392585b0fbbc5b45f1
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-03 12:17:43 +0100

    Created PollProgressMessage class.

---
+++ app/Libraries/CafeVariome/Net/Service/PollProgressMessage.php
@@ -0,0 +1,19 @@
+<?php namespace App\Libraries\CafeVariome\Net\Service;
+
+/**
+ * PollProgressMessage.php
+ * Created 25/07/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class PollProgressMessage extends AbstractMessage
+{
+	protected string $type;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->SetType();
+	}
+}
