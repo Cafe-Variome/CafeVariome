bb55175fce05bb4b91b74694f6f3532c589ebf93
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-17 10:18:38 +0100

    Created DataFileList view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/DataFileList.php
@@ -0,0 +1,24 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+
+class DataFileList extends BaseViewModel
+{
+	public string $name;
+
+	public string $size;
+
+	public string $upload_date;
+
+	public int $record_count;
+
+	public string $status;
+
+	public int $user_id;
+
+	public string $user_username;
+
+	public string $user_first_name;
+
+	public string $user_last_name;
+
+}
