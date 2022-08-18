d65ef4a7192632a913bf416ab9d05c896072233f
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-18 13:28:25 +0100

    Created OntologyDropDown view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/OntologyDropDown.php
@@ -0,0 +1,13 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+/**
+ * OntologyDropDown.php
+ * Created 18/08/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class OntologyDropDown extends BaseViewModel
+{
+	public string $name;
+}
