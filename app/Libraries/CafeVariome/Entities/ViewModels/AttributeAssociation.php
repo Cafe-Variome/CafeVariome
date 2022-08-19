2fba5b0a676c7ac61f623142bf5d6212dad04e5b
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-19 16:58:17 +0100

    Created AttributeAssociation view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/AttributeAssociation.php
@@ -0,0 +1,18 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+/**
+ * AttributeAssociation.php
+ * Created 18/08/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+
+class AttributeAssociation extends BaseViewModel
+{
+	public string $name;
+
+	public int $source_id;
+
+	public string $source_name;
+}
