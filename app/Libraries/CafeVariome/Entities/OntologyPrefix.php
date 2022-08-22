d9d1023785221c1138cf8084a76aa887a1f70918
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-22 16:16:00 +0100

    Created OntologyPrefix entity class.

---
+++ app/Libraries/CafeVariome/Entities/OntologyPrefix.php
@@ -0,0 +1,16 @@
+<?php namespace App\Libraries\CafeVariome\Entities;
+
+/**
+ * OntologyPrefix.php
+ * Created 22/08/2022
+ *
+ * This class extends Entity and implements IEntity.
+ * @author Mehdi Mehtarizadeh
+ */
+
+class OntologyPrefix extends Entity
+{
+	public string $name;
+
+	public int $ontology_id;
+}
