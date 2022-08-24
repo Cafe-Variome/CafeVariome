bcb6d76d06aea6533b19876e6935832c67e6368e
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-24 16:05:20 +0100

    Replaced models with the new data adapter classes.

--- app/Libraries/CafeVariome/Core/DataPipeLine/DataPipeLine.php
+++ app/Libraries/CafeVariome/Core/DataPipeLine/DataPipeLine.php
@@ -11,18 +11,20 @@
 use App\Libraries\CafeVariome\Core\IO\FileSystem\FileMan;
 use App\Libraries\CafeVariome\Database\AttributeAdapter;
 use App\Libraries\CafeVariome\Database\DataFileAdapter;
+use App\Libraries\CafeVariome\Database\EAVAdapter;
 use App\Libraries\CafeVariome\Database\GroupAdapter;
+use App\Libraries\CafeVariome\Database\PipelineAdapter;
 use App\Libraries\CafeVariome\Database\SourceAdapter;
 use App\Libraries\CafeVariome\Database\SubjectAdapter;
 use App\Libraries\CafeVariome\Database\ValueAdapter;
 use App\Libraries\CafeVariome\Factory\AttributeAdapterFactory;
 use App\Libraries\CafeVariome\Factory\DataFileAdapterFactory;
+use App\Libraries\CafeVariome\Factory\EAVAdapterFactory;
 use App\Libraries\CafeVariome\Factory\GroupAdapterFactory;
+use App\Libraries\CafeVariome\Factory\PipelineAdapterFactory;
 use App\Libraries\CafeVariome\Factory\SourceAdapterFactory;
 use App\Libraries\CafeVariome\Factory\SubjectAdapterFactory;
 use App\Libraries\CafeVariome\Factory\ValueAdapterFactory;
-use App\Models\EAV;
-use App\Models\Pipeline;
 
 class DataPipeLine
 {
@@ -34,28 +36,30 @@ class DataPipeLine
 
 	protected string $basePath;
 	protected array $configuration;
+	protected EAVAdapter $EAVAdapter;
 	protected DataFileAdapter $dataFileAdapter;
 	protected SourceAdapter $sourceAdapter;
 	protected SubjectAdapter $subjectAdapter;
 	protected GroupAdapter $groupAdapter;
 	protected AttributeAdapter $attributeAdapter;
 	protected ValueAdapter $valueAdapter;
+	protected PipelineAdapter $piplineAdapter;
 
 	public function __construct(int $source_id)
 	{
 		$this->sourceId = $source_id;
 
+		$this->EAVAdapter = (new EAVAdapterFactory())->GetInstance();
 		$this->dataFileAdapter = (new DataFileAdapterFactory())->GetInstance();
 		$this->sourceAdapter = (new SourceAdapterFactory())->GetInstance();
 		$this->subjectAdapter = (new SubjectAdapterFactory())->GetInstance();
 		$this->groupAdapter = (new GroupAdapterFactory())->GetInstance();
 		$this->attributeAdapter = (new AttributeAdapterFactory())->GetInstance();
 		$this->valueAdapter = (new ValueAdapterFactory())->GetInstance();
+		$this->piplineAdapter = (new PipelineAdapterFactory())->GetInstance();
 
 		$this->basePath = FCPATH . UPLOAD . UPLOAD_DATA . $this->sourceId . DIRECTORY_SEPARATOR;
 
-		$this->eavModel = new EAV();
-		$this->pipelineModel = new Pipeline();
 		$this->fileMan = new FileMan($this->basePath);
 	}
 
@@ -63,15 +67,15 @@ class DataPipeLine
 	{
 		$db = \Config\Database::connect();
 		$db->transStart();
-		$valueFrequencies = $this->eavModel->getValueFrequenciesBySourceIdAndFileId($this->sourceId, $file_id); // Get value frequencies
+		$valueFrequencies = $this->EAVAdapter->ReadValueFrequenciesBySourceIdAndFileId($this->sourceId, $file_id); // Get value frequencies
 		if (count($valueFrequencies) > 0)
 		{
-			$this->eavModel->deleteRecordsByFileId($file_id); // Delete records from eavs
+			$this->EAVAdapter->DeleteByFileId($file_id); // Delete records from eavs
 			$valueCount = count($valueFrequencies);
 			for ($i = 0; $i < $valueCount; $i++)
 			{
-				$value_id = $valueFrequencies[$i]['value_id'];
-				$value_frequency = $valueFrequencies[$i]['frequency'];
+				$value_id = $valueFrequencies[$i]->value_id;
+				$value_frequency = $valueFrequencies[$i]->frequency;
 				$this->valueAdapter->UpdateFrequency($value_id, -$value_frequency);
 				$this->valueAdapter->DeleteIfAbsent($value_id); // Delete value if frequency is 0
 				unset($valueFrequencies[$i]);
