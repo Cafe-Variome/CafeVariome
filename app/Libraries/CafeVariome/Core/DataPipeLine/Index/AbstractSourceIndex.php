bb207af5a28d7f2433f589ff9d1699f3b39f4a5d
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-24 16:00:57 +0100

    Replaced models with the new data adapter layer classes.

--- app/Libraries/CafeVariome/Core/DataPipeLine/Index/AbstractSourceIndex.php
+++ app/Libraries/CafeVariome/Core/DataPipeLine/Index/AbstractSourceIndex.php
@@ -1,10 +1,5 @@
 <?php namespace App\Libraries\CafeVariome\Core\DataPipeLine\Index;
 
-use App\Libraries\CafeVariome\Net\ServiceInterface;
-use App\Models\Attribute;
-use App\Models\EAV;
-use App\Models\Source;
-
 /**
  * Name AbstractSourceIndex.php
  *
@@ -13,19 +8,39 @@ use App\Models\Source;
  *
  */
 
+use App\Libraries\CafeVariome\Database\AttributeAdapter;
+use App\Libraries\CafeVariome\Database\EAVAdapter;
+use App\Libraries\CafeVariome\Database\SourceAdapter;
+use App\Libraries\CafeVariome\Database\SubjectAdapter;
+use App\Libraries\CafeVariome\Database\ValueAdapter;
+use App\Libraries\CafeVariome\Entities\ViewModels\AttributeNameType;
+use App\Libraries\CafeVariome\Entities\ViewModels\ValueName;
+use App\Libraries\CafeVariome\Factory\AttributeAdapterFactory;
+use App\Libraries\CafeVariome\Factory\EAVAdapterFactory;
+use App\Libraries\CafeVariome\Factory\SourceAdapterFactory;
+use App\Libraries\CafeVariome\Factory\SubjectAdapterFactory;
+use App\Libraries\CafeVariome\Factory\ValueAdapterFactory;
+use App\Libraries\CafeVariome\Net\ServiceInterface;
+
 abstract class AbstractSourceIndex
 {
-	protected Attribute $attributeModel;
-	protected bool $append;
+	protected bool $continue;
+	protected int $taskId;
+	protected AttributeAdapter $attributeAdapter;
+	protected ValueAdapter $valueAdapter;
+	protected bool $overwrite;
 	protected $dbInstance;
 	protected string $jobName;
 	protected int $sourceId;
 	protected string $sourceName;
 	protected string $sourceUID;
-	protected EAV $eavModel;
+	protected EAVAdapter $EAVAdapter;
+
 	protected ServiceInterface $serviceInterface;
-	protected Source $sourceModel;
+	protected SourceAdapter $sourceAdapter;
+	protected SubjectAdapter $subjectAdapter;
 	protected array $attributes;
+	protected array $subjects;
 	protected int $totalEAVsCount;
 	protected int $totalRecords;
 	protected int $processedRecords;
@@ -33,71 +48,117 @@ abstract class AbstractSourceIndex
 	public function __construct(int $source_id)
 	{
 		$this->sourceId = $source_id;
-		$this->attributeModel = new Attribute();
-		$this->eavModel = new EAV();
+		$this->attributeAdapter = (new AttributeAdapterFactory())->GetInstance();
+		$this->valueAdapter = (new ValueAdapterFactory())->GetInstance();
+		$this->EAVAdapter = (new EAVAdapterFactory())->GetInstance();
 		$this->serviceInterface = new ServiceInterface();
-		$this->sourceModel = new Source();
+		$this->sourceAdapter = (new SourceAdapterFactory())->GetInstance();
+		$this->subjectAdapter = (new SubjectAdapterFactory())->GetInstance();
 		$this->attributes = [];
+		$this->subjects = [];
+		$this->initiateSource();
 	}
 
 	abstract public function IndexSource();
 
-	protected function getAttributeNameById(int $attribute_id): ?string
+	protected function getSubjectById(int $subject_id): ?string
 	{
-		if (array_key_exists($attribute_id, $this->attributes)){
-			return $this->attributes[$attribute_id]['name'];
+		if (array_key_exists($subject_id, $this->subjects))
+		{
+			return $this->subjects[$subject_id];
 		}
-		else{
-			$attribute = $this->attributeModel->getAttributeAndValues($attribute_id);
-			if ($attribute != null){
-				$this->attributes[$attribute_id] = $attribute;
-				return $attribute['name'];
+		else
+		{
+			$subject = $this->subjectAdapter->Read($subject_id);
+			if (!$subject->isNull())
+			{
+				$this->subjects[$subject->name] = $subject->name;
+				return $subject->name;
 			}
 		}
+
+		return null;
+	}
+
+	protected function getAttributeNameById(int $attribute_id): ?string
+	{
+		if (array_key_exists($attribute_id, $this->attributes))
+		{
+			return $this->attributes[$attribute_id]['name'];
+		}
+		else
+		{
+			$attribute = $this->attributeAdapter->SetModel(AttributeNameType::class)->Read($attribute_id);
+			if (!$attribute->isNull())
+			{
+				$values = $this->valueAdapter->SetModel(ValueName::class)->ReadByAttributeId($attribute_id);
+				$this->attributes[$attribute_id] = [
+					'name' => $attribute->name,
+					'type' => $attribute->type,
+					'values' => $values
+				];
+				return $attribute->name;
+			}
+		}
+
 		return null;
 	}
 
 	protected function getAttributeById(int $attribute_id): ?array
 	{
-		if (array_key_exists($attribute_id, $this->attributes)){
+		if (array_key_exists($attribute_id, $this->attributes))
+		{
 			return $this->attributes[$attribute_id];
 		}
-		else{
-			$attribute = $this->attributeModel->getAttributeAndValues($attribute_id);
-			if ($attribute != null){
-				$this->attributes[$attribute_id] = $attribute;
-				return $attribute;
+		else
+		{
+			$attribute = $this->attributeAdapter->SetModel(AttributeNameType::class)->Read($attribute_id);
+			if (!$attribute->isNull())
+			{
+				$values = $this->valueAdapter->SetModel(ValueName::class)->ReadByAttributeId($attribute_id);
+				$this->attributes[$attribute_id] = [
+					'name' => $attribute->name,
+					'type' => $attribute->type,
+					'values' => $values
+				];
+				return $this->attributes[$attribute_id];
 			}
 		}
+
 		return null;
 	}
 
 	protected function getValueNameByIdAndAttributeId(int $value_id, int $attribute_id): ?string
 	{
-		if (array_key_exists($attribute_id, $this->attributes)){
+		if (array_key_exists($attribute_id, $this->attributes))
+		{
 			$values = $this->attributes[$attribute_id]['values'];
-			if(array_key_exists($value_id, $values)){
-				return $values[$value_id];
+			if(array_key_exists($value_id, $values))
+			{
+				return $values[$value_id]->name;
 			}
 		}
+
 		return null;
 	}
 
 	protected function initiateSource()
 	{
-		$source = $this->sourceModel->getSource($this->sourceId);
-		if ($source){
-			$this->sourceName = $source['name'];
-			$this->sourceUID = $source['uid'];
+		$source = $this->sourceAdapter->Read($this->sourceId);
+		if (!$source->isNull())
+		{
+			$this->sourceName = $source->name;
+			$this->sourceUID = $source->uid;
 		}
-		else{
+		else
+		{
 			throw new \Exception('Source does not exist');
 		}
 	}
 
-	protected function registerProcess(int $source_id, string $message ='Starting')
+	protected function CalculateProgressInPercent(): int
 	{
-		$this->serviceInterface->RegisterProcess($source_id, 1, $this->jobName, $message);
+		return intval(ceil((($this->processedRecords / $this->totalRecords)) * 100.0));
 	}
 
 	protected function ReportProgress(?int $progress = null, string $status = '', bool $finished = false)
