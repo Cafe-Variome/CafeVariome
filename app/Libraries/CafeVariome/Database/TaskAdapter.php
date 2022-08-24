a3dba6a33729f56f0f419b9d7e448d10d88c00c8
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-24 16:07:05 +0100

    Added ReadLastProcessingTaskIdBySourceIdAndType() function.

--- app/Libraries/CafeVariome/Database/TaskAdapter.php
+++ app/Libraries/CafeVariome/Database/TaskAdapter.php
@@ -23,6 +23,25 @@ class TaskAdapter extends BaseAdapter
 	 */
 	protected static string $key = 'id';
 
+	public function ReadLastProcessingTaskIdBySourceIdAndType(int $source_id, int $task_type): ?int
+	{
+		$this->builder->select(static::$table . '.' . static::$key);
+		$this->builder->where(static::$table . '.source_id', $source_id);
+		$this->builder->where(static::$table . '.type', $task_type);
+		$this->builder->where(static::$table . '.status', TASK_STATUS_PROCESSING);
+		$this->builder->limit(1);
+		$this->builder->orderBy(static::$table . '.' . static::$key, 'DESC');
+
+
+		$result = $this->builder->get()->getResult();
+
+		if (count($result) == 1)
+		{
+			return $result[0]->id;
+		}
+		return null;
+	}
+
     /**
      * @inheritDoc
      */
