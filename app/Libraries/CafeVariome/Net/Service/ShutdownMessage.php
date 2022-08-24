3ee0278e36ac69ccf57f59d2c8f10d879f5e0147
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-24 15:46:00 +0100

    Created ShutdownMessage class.

---
+++ app/Libraries/CafeVariome/Net/Service/ShutdownMessage.php
@@ -0,0 +1,16 @@
+<?php namespace App\Libraries\CafeVariome\Net\Service;
+
+/**
+ * ShutdownMessage.php
+ * Created 24/08/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class ShutdownMessage extends AbstractMessage
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
