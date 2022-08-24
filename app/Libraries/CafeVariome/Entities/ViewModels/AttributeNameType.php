9c0f80a1ca6f2e26502b044ecb0fc031f17632d0
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-24 15:41:57 +0100

    Created AttributeNameType view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/AttributeNameType.php
@@ -0,0 +1,16 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+/**
+ * AttributeNameType.php
+ * Created 23/08/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class AttributeNameType extends BaseViewModel
+{
+	public string $name;
+
+	public int $type;
+
+}
