476f193465e9b237ca321ac0f7cf7f8ca5bd4ef4
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-08-24 15:47:24 +0100

    Created ValueName view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/ValueName.php
@@ -0,0 +1,13 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+/**
+ * ValueName.php
+ * Created 23/08/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class ValueName extends BaseViewModel
+{
+	public string $name;
+}
