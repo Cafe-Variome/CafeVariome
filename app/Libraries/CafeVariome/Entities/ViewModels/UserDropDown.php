798ec78f7bdc4d2390faff76fb529d4d9cb79ce2
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-07 11:48:14 +0100

    Created UserDropDown view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/UserDropDown.php
@@ -0,0 +1,17 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+/**
+ * UserDropDown.php
+ * Created 06/09/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class UserDropDown extends BaseViewModel
+{
+	public string $first_name;
+
+	public string $last_name;
+
+	public string $email;
+}
