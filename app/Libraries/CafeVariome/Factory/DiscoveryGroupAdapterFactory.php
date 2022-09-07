baa212913eb565983a5e64e052f143ebc20f5d85
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-07 11:49:18 +0100

    Created DiscoveryGroupAdapterFactory class.

---
+++ app/Libraries/CafeVariome/Factory/DiscoveryGroupAdapterFactory.php
@@ -0,0 +1,23 @@
+<?php namespace App\Libraries\CafeVariome\Factory;
+
+/**
+ * DiscoveryGroupAdapterFactory.php
+ * Created 06/09/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+
+use App\Libraries\CafeVariome\Database\DiscoveryGroupAdapter;
+use App\Libraries\CafeVariome\Database\IAdapter;
+
+class DiscoveryGroupAdapterFactory extends AdapterFactory
+{
+    /**
+     * @inheritDoc
+     */
+    public function GetInstance(): IAdapter
+    {
+        return new DiscoveryGroupAdapter();
+    }
+}
