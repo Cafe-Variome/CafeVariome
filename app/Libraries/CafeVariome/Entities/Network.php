3399c24b50a6753bf8a36fda8b583dc869801348
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-07 11:49:47 +0100

    Created Network entity class.

---
+++ app/Libraries/CafeVariome/Entities/Network.php
@@ -0,0 +1,14 @@
+<?php namespace App\Libraries\CafeVariome\Entities;
+
+/**
+ * Network.php
+ * Created 25/08/2022
+ *
+ * This class extends Entity and implements IEntity.
+ * @author Mehdi Mehtarizadeh
+ */
+
+class Network extends Entity
+{
+	public string $name;
+}
