f953d5de0aff2850352e307cb8c96052e5baa8f4
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-07 11:48:24 +0100

    Created SourceDropDown view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/SourceDropDown.php
@@ -0,0 +1,13 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+/**
+ * SourceDropDown.php
+ * Created 06/09/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class SourceDropDown extends BaseViewModel
+{
+	public string $name;
+}
