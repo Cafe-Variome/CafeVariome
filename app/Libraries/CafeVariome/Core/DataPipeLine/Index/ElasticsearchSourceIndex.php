9732b7c1d4d0d24f645003a767b43c3bbdf72bfb
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-08 16:23:47 +0100

    Replaced models with data adapter layer classes.

--- app/Libraries/CafeVariome/Core/DataPipeLine/Index/ElasticsearchSourceIndex.php
+++ app/Libraries/CafeVariome/Core/DataPipeLine/Index/ElasticsearchSourceIndex.php
@@ -9,17 +9,20 @@
  *
  */
 
+use App\Libraries\CafeVariome\CafeVariome;
+use App\Libraries\CafeVariome\Entities\Task;
 use App\Libraries\CafeVariome\Helpers\Core\ElasticsearchHelper;
-use \App\Models\Settings;
 
 class ElasticsearchSourceIndex extends AbstractSourceIndex
 {
-	public function __construct(int $source_id, bool $append)
+	public function __construct(Task $task)
 	{
-		parent::__construct($source_id);
-		$this->append = $append;
+		parent::__construct($task->source_id);
+		$this->continue = true;
+		$this->overwrite = $task->overwrite;
+		$this->taskId = $task->getID();
+
 		$this->dbInstance =  new ElasticSearch([$this->getEndPointURI()]);
-		$this->jobName = 'elasticsearchindex';
 		$this->processedRecords = 0;
 		$this->totalRecords = 0;
 		$this->totalEAVsCount = 0;
@@ -27,84 +30,93 @@ class ElasticsearchSourceIndex extends AbstractSourceIndex
 
     public function IndexSource()
     {
-		$this->registerProcess($this->sourceId);
+		$index_name = ElasticsearchHelper::GetSourceIndexName($this->sourceId);
 
-		$index_name = ElasticsearchHelper::getSourceIndexName($this->sourceId);
-
-		if (!$this->append){
+		if ($this->overwrite)
+		{
 			$this->dbInstance->deleteIndex($index_name);
 		}
 
-		if (!$this->dbInstance->indexExists($index_name)){
+		if (!$this->dbInstance->indexExists($index_name))
+		{
 			$this->dbInstance->createIndex($index_name, $this->getMapping());
 		}
 
 		//Get attribute Ids that must be stored on Elasticsearch
-		$attribute_ids = $this->attributeModel->getAttributeIdsBySourceIdAndStorageLocation($this->sourceId, ATTRIBUTE_STORAGE_ELASTICSEARCH);
+		$attribute_ids = $this->attributeAdapter->ReadIdsBySourceIdAndStorageLocation($this->sourceId, ATTRIBUTE_STORAGE_ELASTICSEARCH);
+
+		$this->sourceAdapter->Lock($this->sourceId);
 
 		$this->createParentDocuments($attribute_ids);
 		$this->createChildDocuments($attribute_ids);
 
-		$this->eavModel->setIndexedFlagBySourceIdAndAttributeIds($this->sourceId, $attribute_ids);
-		$this->sourceModel->unlockSource($this->sourceId);
+		$this->EAVAdapter->UpdateIndexedBySourceIdAndAttributeIds($this->sourceId, $attribute_ids);
 
-		$this->reportProgress($this->sourceId, 1, 1, 'Finished', true);
+		$this->sourceAdapter->Unlock($this->sourceId);
+
+		$this->ReportProgress(100, 'Finished', true);
 	}
 
 	private function createParentDocuments(array $attribute_ids)
 	{
-		$index_name = ElasticsearchHelper::getSourceIndexName($this->sourceId);
+		$index_name = ElasticsearchHelper::GetSourceIndexName($this->sourceId);
 		$bulk = [];
 		$offset = 0;
 		$currId = 0;
 		$batchSize = EAV_BATCH_SIZE;
 
 		// Get total EAV records by source_id
-		$this->totalEAVsCount = $this->eavModel->countEAVsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $this->append);
+		$this->totalEAVsCount = $this->EAVAdapter->CountBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, !$this->overwrite);
 		// Get unique subject_ids by source_id
-		$uniqueUIDsCount = $this->eavModel->countUniqueUIDsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $this->append);
+		$uniqueUIDsCount = $this->EAVAdapter->CountUniqueGroupsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, !$this->overwrite);
 		//Total number of Elasticsearch documents to be created.
 		$this->totalRecords = $this->totalEAVsCount + $uniqueUIDsCount;
 
-		$this->reportProgress($this->sourceId, $this->processedRecords, $this->totalRecords, 'Generating Elasticsearch index');
+		$this->ReportProgress(0, 'Generating Elasticsearch index');
 
 		while ($offset < $uniqueUIDsCount)
 		{
-			$unique_ids = $this->eavModel->getUniqueUIDsAndSubjectIdsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $batchSize, $currId, $this->append);
+			if (is_null($currId)) break;
+
+			$unique_ids = $this->EAVAdapter->ReadUniqueGroupIdsAndSubjectIdsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $batchSize, $currId, !$this->overwrite);
 			$uid_count = count($unique_ids);
 
-			if ($uid_count == 0){
+			if ($uid_count == 0)
+			{
 				break;
 			}
 
-			$currId = $this->eavModel->getLastIdByUID($unique_ids[$uid_count - 1]['uid']);
+			$currId = $this->EAVAdapter->ReadLastIdByGroupIdAndSubjectId($unique_ids[$uid_count - 1]->subject_id, $unique_ids[$uid_count - 1]->group_id);
 
 			// start making all the parent documents in ElasticSearch
-			for ($i = 0; $i < $uid_count; $i++) {
+			for ($i = 0; $i < $uid_count; $i++)
+			{
 				$bulk['body'][] = [
 					'index'=>[
-						'_id' => $unique_ids[$i]['uid'],
+						'_id' => $unique_ids[$i]->subject_id . '_' . $unique_ids[$i]->group_id,
 						'_index' => $index_name
 					]
 				];
 				$bulk['body'][] = [
-					'subject_id' => $unique_ids[$i]['subject_id'],
+					'subject_id' => $this->getSubjectById($unique_ids[$i]->subject_id),
 					'eav_rel' => [
 						'name' => 'sub'
 					],
 					'type' => 'sub',
 					'source_id' => $this->sourceId,
-					'file_id' => $unique_ids[$i]['file_id']
+					'file_id' => $unique_ids[$i]->data_file_id
 				];
 
 				unset($unique_ids[$i]);
 				$this->processedRecords++;
 
-				if ($this->processedRecords % EAV_BATCH_SIZE == 0){
+				if ($this->processedRecords % EAV_BATCH_SIZE == 0)
+				{
 					$responses = $this->dbInstance->indexDocuments($bulk);
 					$bulk = [];
 					unset ($responses);
-					$this->reportProgress($this->sourceId, $this->processedRecords, $this->totalRecords);
+
+					$this->ReportProgress();
 				}
 			}
 
@@ -112,66 +124,73 @@ class ElasticsearchSourceIndex extends AbstractSourceIndex
 		}
 
 		// Send the last documents through, if any
-		if (!empty($bulk['body'])){
+		if (!empty($bulk['body']))
+		{
 			$responses = $this->dbInstance->indexDocuments($bulk);
 			$bulk = [];
 			unset ($responses);
-			$this->reportProgress($this->sourceId, $this->processedRecords, $this->totalRecords);
+
+			$this->ReportProgress();
 		}
 	}
 
 	private function createChildDocuments(array $attribute_ids)
 	{
-		$index_name = ElasticsearchHelper::getSourceIndexName($this->sourceId);
+		$index_name = ElasticsearchHelper::GetSourceIndexName($this->sourceId);
 		$bulk=[];
 		$offset = 0;
 		$currId = 0;
 		$batchSize = EAV_BATCH_SIZE;
 
-		while ($offset < $this->totalEAVsCount){
+		while ($offset < $this->totalEAVsCount)
+		{
 			// Get current limit chunk of data
-			$eavdata = $this->eavModel->getEAVsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $batchSize, $currId, $this->append);
+			$eavdata = $this->EAVAdapter->ReadBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $batchSize, $currId, !$this->overwrite);
 			$eav_count = count($eavdata);
 			$lastRec = end($eavdata);
-			$currId = $lastRec['id'];
+			$currId = $lastRec->id;
 			reset($eavdata);
 
 			// Loop through limit chunk
-			for ($i = 0; $i < $eav_count; $i++) {
-				//$eavdata[$i]['attribute'] = preg_replace('/\s+/', '_', $eavdata[$i]['attribute']);
-				$attribute_id = $eavdata[$i]['attribute_id'];
-				$value_id = $eavdata[$i]['value_id'];
+			for ($i = 0; $i < $eav_count; $i++)
+			{
+				$attribute_id = $eavdata[$i]->attribute_id;
+				$value_id = $eavdata[$i]->value_id;
+				$subject_id = $eavdata[$i]->subject_id;
+				$group_id = $eavdata[$i]->group_id;
+				$subject_name = $this->getSubjectById($subject_id);
 				$attribute_name = $this->getAttributeNameById($attribute_id);
 				$value_name = $this->getValueNameByIdAndAttributeId($value_id, $attribute_id);
 
-				$bulk['routing'] = $eavdata[$i]['uid'];
+				$bulk['routing'] = $subject_id . '_' . $group_id;
 				$bulk['body'][] = [
 					'index' => [
 						'_index' => $index_name
 					]
 				];
 				$bulk['body'][] = [
-					'subject_id' => $eavdata[$i]['subject_id'],
+					'subject_id' => $subject_name,
 					'attribute' => $attribute_name,
 					'value' => strtolower($value_name),
 					'eav_rel' => [
 						'name' => 'eav',
-						'parent' => $eavdata[$i]['uid']
+						'parent' => $subject_id . '_' . $group_id
 					],
 					'type' => 'eav',
 					'source_id' => $this->sourceId,
-					'file_id' => $eavdata[$i]['file_id']
+					'file_id' => $eavdata[$i]->data_file_id
 				];
 
 				unset($eavdata[$i]);
 				$this->processedRecords++;
 
-				if ($this->processedRecords % EAV_BATCH_SIZE == 0){
+				if ($this->processedRecords % EAV_BATCH_SIZE == 0)
+				{
 					$responses = $this->dbInstance->indexDocuments($bulk);
 					$bulk=[];
 					unset ($responses);
 
-					$this->reportProgress($this->sourceId, $this->processedRecords, $this->totalRecords);
+					$this->ReportProgress();
 				}
 			}
 
@@ -180,9 +199,11 @@ class ElasticsearchSourceIndex extends AbstractSourceIndex
 		}
 
 		// Send the last set of documents through
-		if (!empty($bulk['body'])){
+		if (!empty($bulk['body']))
+		{
 			$responses = $this->dbInstance->indexDocuments($bulk);
-			$this->reportProgress($this->sourceId, $this->processedRecords, $this->totalRecords);
+
+			$this->ReportProgress();
 			unset ($responses);
 		}
 	}
@@ -215,7 +236,7 @@ class ElasticsearchSourceIndex extends AbstractSourceIndex
 
 	private function getEndPointURI()
 	{
-		$setting = Settings::getInstance();
-		return $setting->getElasticSearchUri();
+		$setting = CafeVariome::Settings();
+		return $setting->GetElasticSearchUri();
 	}
 }
--- app/Libraries/CafeVariome/Core/DataPipeLine/Index/Neo4JSourceIndex.php
+++ app/Libraries/CafeVariome/Core/DataPipeLine/Index/Neo4JSourceIndex.php
@@ -1,8 +1,13 @@
 <?php namespace App\Libraries\CafeVariome\Core\DataPipeLine\Index;
 
-use App\Models\Ontology;
-use App\Models\OntologyPrefix;
-use App\Models\OntologyRelationship;
+use App\Libraries\CafeVariome\Database\OntologyAdapter;
+use App\Libraries\CafeVariome\Database\OntologyPrefixAdapter;
+use App\Libraries\CafeVariome\Database\OntologyRelationshipAdapter;
+use App\Libraries\CafeVariome\Entities\IEntity;
+use App\Libraries\CafeVariome\Entities\Task;
+use App\Libraries\CafeVariome\Factory\OntologyAdapterFactory;
+use App\Libraries\CafeVariome\Factory\OntologyPrefixAdapterFactory;
+use App\Libraries\CafeVariome\Factory\OntologyRelationshipAdapterFactory;
 
 /**
  * Name Neo4JDataIndex.php
@@ -15,27 +20,29 @@ use App\Models\OntologyRelationship;
 
 class Neo4JSourceIndex extends AbstractSourceIndex
 {
-	private Ontology $ontologyModel;
-	private OntologyPrefix $prefixModel;
-	private OntologyRelationship $relationshipModel;
+	private OntologyAdapter $ontologyAdapter;
+	private OntologyPrefixAdapter $ontologyPrefixAdapter;
+	private OntologyRelationshipAdapter $ontologyRelationshipAdapter;
+
 	private array $ontologies;
 	private array $ontologyPrefixes;
 	private array $ontologyRelationships;
 	private array $ontologyAssociations;
 
-	public function __construct(int $source_id, bool $append)
+	public function __construct(Task $task)
 	{
-		parent::__construct($source_id);
-		$this->initiateSource();
-		$this->append = $append;
+		parent::__construct($task->source_id);
+		$this->continue = true;
+		$this->overwrite = $task->overwrite;
+		$this->taskId = $task->getID();
+
 		$this->dbInstance = new Neo4J();
-		$this->jobName = 'neo4jindex';
 		$this->processedRecords = 0;
 		$this->totalRecords = 0;
 		$this->totalEAVsCount = 0;
-		$this->ontologyModel = new Ontology();
-		$this->prefixModel = new OntologyPrefix();
-		$this->relationshipModel = new OntologyRelationship();
+		$this->ontologyAdapter = (new OntologyAdapterFactory())->GetInstance();
+		$this->ontologyPrefixAdapter = (new OntologyPrefixAdapterFactory())->GetInstance();
+		$this->ontologyRelationshipAdapter = (new OntologyRelationshipAdapterFactory())->GetInstance();
 		$this->ontologies = [];
 		$this->ontologyPrefixes = [];
 		$this->ontologyRelationships = [];
@@ -44,22 +51,24 @@ class Neo4JSourceIndex extends AbstractSourceIndex
 
     public function IndexSource()
     {
-		$this->registerProcess($this->sourceId);
-
-		if (!$this->append) {
+		if ($this->overwrite)
+		{
 			$this->dbInstance->deleteSource($this->sourceId);
 		}
 
 		//Get attribute Ids that must be stored on Elasticsearch
-		$attribute_ids = $this->attributeModel->getAttributeIdsBySourceIdAndStorageLocation($this->sourceId, ATTRIBUTE_STORAGE_NEO4J);
+		$attribute_ids = $this->attributeAdapter->ReadIdsBySourceIdAndStorageLocation($this->sourceId, ATTRIBUTE_STORAGE_NEO4J);
+
+		$this->sourceAdapter->Lock($this->sourceId);
 
 		$this->createSubjectNodes($attribute_ids);
 		$this->createRelationships($attribute_ids);
 
-		$this->eavModel->setIndexedFlagBySourceIdAndAttributeIds($this->sourceId, $attribute_ids);
-		$this->sourceModel->unlockSource($this->sourceId);
+		$this->EAVAdapter->UpdateIndexedBySourceIdAndAttributeIds($this->sourceId, $attribute_ids);
 
-		$this->reportProgress($this->sourceId, 1, 1, 'Finished', true);
+		$this->sourceAdapter->Unlock($this->sourceId);
+
+		$this->ReportProgress(100, 'Finished', true);
     }
 
 	private function createSubjectNodes(array $attribute_ids)
@@ -69,32 +78,34 @@ class Neo4JSourceIndex extends AbstractSourceIndex
 		$batchSize = NEO4J_BATCH_SIZE;
 
 		// Get total EAV records by source_id (Neo4J relationships)
-		$this->totalEAVsCount = $this->eavModel->countEAVsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $this->append);
+		$this->totalEAVsCount = $this->EAVAdapter->CountBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, !$this->overwrite);
 		// Get unique subject_ids by source_id
-		$uniqueSubjectIdsCount = $this->eavModel->countUniqueSubjectIdsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $this->append);
+		$uniqueSubjectIdsCount = $this->EAVAdapter->CountUniqueSubjectIdsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, !$this->overwrite);
 		//Total number of records to be created.
 		$this->totalRecords = $this->totalEAVsCount + $uniqueSubjectIdsCount;
 
-		$this->reportProgress($this->sourceId, $this->processedRecords, $this->totalRecords, 'Inserting subjects into Neo4J');
+		$this->ReportProgress(0, 'Inserting subjects into Neo4J');
 
 		while ($offset < $uniqueSubjectIdsCount)
 		{
-			$unique_subject_ids = $this->eavModel->getUniqueSubjectIdsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $batchSize, $currId, $this->append);
+			$unique_subject_ids = $this->EAVAdapter->ReadUniqueSubjectIdsAndFileIdsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $batchSize, $currId, !$this->overwrite);
 			$subject_id_count = count($unique_subject_ids);
 
-			if ($subject_id_count == 0){
+			if ($subject_id_count == 0)
+			{
 				break;
 			}
 
-			$currId = $this->eavModel->getLastIdBySubjectId($unique_subject_ids[$subject_id_count - 1]['subject_id']);
+			$currId = $this->EAVAdapter->ReadLastIdBySubjectId($unique_subject_ids[$subject_id_count - 1]->subject_id);
 
 			for($i = 0; $i < $subject_id_count; $i++)
 			{
-				$this->dbInstance->InsertSubject($unique_subject_ids[$i]['subject_id'], $this->sourceId, $unique_subject_ids[$i]['file_id'], $this->sourceUID);
+				$subject_id = $this->getSubjectById($unique_subject_ids[$i]->subject_id);
+				$this->dbInstance->InsertSubject($subject_id, $this->sourceId, $unique_subject_ids[$i]->data_file_id, $this->sourceUID);
 				unset($unique_subject_ids[$i]);
 				$this->processedRecords++;
 
-				$this->reportProgress($this->sourceId, $this->processedRecords, $this->totalRecords);
+				$this->ReportProgress();
 			}
 
 			$this->dbInstance->commitTransaction(true);
@@ -109,94 +120,110 @@ class Neo4JSourceIndex extends AbstractSourceIndex
 		$currId = 0;
 		$batchSize = NEO4J_BATCH_SIZE;
 
-		while ($offset < $this->totalEAVsCount) {
-
-			$eavdata = $this->eavModel->getEAVsBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $batchSize, $currId, $this->append);
+		while ($offset < $this->totalEAVsCount)
+		{
+			$eavdata = $this->EAVAdapter->ReadBySourceIdAndAttributeIds($this->sourceId, $attribute_ids, $batchSize, $currId, !$this->overwrite);
 			$eav_count = count($eavdata);
-			if ($eav_count == 0){
+			if ($eav_count == 0)
+			{
 				break;
 			}
 			$lastRec = end($eavdata);
-			$currId = $lastRec['id'];
+			$currId = $lastRec->id;
 			reset($eavdata);
 
-			for ($i = 0; $i < $eav_count; $i++) {
-				$subject_id = $eavdata[$i]['subject_id'];
-				$attribute_id = $eavdata[$i]['attribute_id'];
-				$value_id = $eavdata[$i]['value_id'];
+			for ($i = 0; $i < $eav_count; $i++)
+			{
+				$subject_id = $this->getSubjectById($eavdata[$i]->subject_id);
+				$attribute_id = $eavdata[$i]->attribute_id;
+				$value_id = $eavdata[$i]->value_id;
 				$attribute = $this->getAttributeById($attribute_id);
 				$value_name = $this->getValueNameByIdAndAttributeId($value_id, $attribute_id);
 
-				if ($attribute != null && $attribute['type'] == ATTRIBUTE_TYPE_ONTOLOGY_TERM){
+				if ($attribute != null && $attribute['type'] == ATTRIBUTE_TYPE_ONTOLOGY_TERM)
+				{
 					$ontologyAssociations = $this->getOntologyAssociationsByAttributeId($attribute_id);
-					foreach ($ontologyAssociations as $ontologyAssoctiaion) {
-						$prefix = $this->getOntologyPrefixById($ontologyAssoctiaion['prefix_id']);
+					foreach ($ontologyAssociations as $ontologyAssoctiaion)
+					{
+						$prefix = $this->getOntologyPrefixById($ontologyAssoctiaion->prefix_id);
 
-						if (str_starts_with($value_name, strtolower($prefix))){
-							$ontology = $this->getOntologyById($ontologyAssoctiaion['ontology_id']);
-							$relationship = $this->getOntologyRelationshipById($ontologyAssoctiaion['relationship_id']);
-							$ontology_term = str_replace(strtolower($prefix), $ontology['key_prefix'], $value_name);
-							$this->dbInstance->ConnectSubject($subject_id, $ontology['node_type'], $ontology['node_key'], $ontology_term, $relationship);
+						if (str_starts_with($value_name, strtolower($prefix)))
+						{
+							$ontology = $this->getOntologyById($ontologyAssoctiaion->ontology_id);
+							$relationship = $this->getOntologyRelationshipById($ontologyAssoctiaion->relationship_id);
+							$ontology_term = str_replace(strtolower($prefix), $ontology->key_prefix, $value_name);
+							$this->dbInstance->ConnectSubject($subject_id, $ontology->node_type, $ontology->node_key, $ontology_term, $relationship);
 						}
 					}
 				}
 				unset($eavdata[$i]);
 				$this->processedRecords++;
 			}
+
 			$this->dbInstance->commitTransaction(true);
-			$this->reportProgress($this->sourceId, $this->processedRecords, $this->totalRecords);
+			$this->ReportProgress();
 		}
 	}
 
 	protected function getOntologyAssociationsByAttributeId(int $attribute_id): ?array
 	{
-		if(array_key_exists($attribute_id, $this->ontologyAssociations)){
+		if(array_key_exists($attribute_id, $this->ontologyAssociations))
+		{
 			return $this->ontologyAssociations[$attribute_id];
 		}
-		else{
-			$ontologyAssociations = $this->attributeModel->getOntologyAssociationsByAttributeId($attribute_id);
+		else
+		{
+			$ontologyAssociations = $this->ontologyAdapter->ReadOntologyAssociationsByAttributeId($attribute_id);
 			$this->ontologyAssociations[$attribute_id] = $ontologyAssociations;
 			return $ontologyAssociations;
 		}
+
 		return null;
 	}
 
-	protected function getOntologyById(int $ontology_id): ?array
+	protected function getOntologyById(int $ontology_id): IEntity
 	{
-		if (array_key_exists($ontology_id, $this->ontologies)){
+		if (array_key_exists($ontology_id, $this->ontologies))
+		{
 			return $this->ontologies[$ontology_id];
 		}
-		else{
-			$ontology = $this->ontologyModel->getOntology($ontology_id);
+		else
+		{
+			$ontology = $this->ontologyAdapter->Read($ontology_id);
 			$this->ontologies[$ontology_id] = $ontology;
 			return $ontology;
 		}
-		return null;
 	}
 
 	protected function getOntologyPrefixById(int $prefix_id): ?string
 	{
-		if (array_key_exists($prefix_id, $this->ontologyPrefixes)){
+		if (array_key_exists($prefix_id, $this->ontologyPrefixes))
+		{
 			return $this->ontologyPrefixes[$prefix_id];
 		}
-		else{
-			$prefix = $this->prefixModel->getOntologyPrefix($prefix_id);
-			$this->ontologyPrefixes[$prefix_id] = $prefix['name'];
-			return $prefix['name'];
+		else
+		{
+			$prefix = $this->ontologyPrefixAdapter->Read($prefix_id);
+			$this->ontologyPrefixes[$prefix_id] = $prefix->name;
+			return $prefix->name;
 		}
+
 		return null;
 	}
 
 	protected function getOntologyRelationshipById(int $relationship_id): ?string
 	{
-		if (array_key_exists($relationship_id, $this->ontologyRelationships)){
+		if (array_key_exists($relationship_id, $this->ontologyRelationships))
+		{
 			return $this->ontologyRelationships[$relationship_id];
 		}
-		else{
-			$relationship = $this->relationshipModel->getOntologyRelationship($relationship_id);
-			$this->ontologyRelationships[$relationship_id] = $relationship['name'];
-			return $relationship['name'];
+		else
+		{
+			$relationship = $this->ontologyRelationshipAdapter->Read($relationship_id);
+			$this->ontologyRelationships[$relationship_id] = $relationship->name;
+			return $relationship->name;
 		}
+
 		return null;
 	}
 }
