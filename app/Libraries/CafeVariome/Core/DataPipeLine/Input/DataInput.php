f5caf15aeea022708317bc513ad49373aeccfaf0
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-08 15:19:28 +0100

    Fixed a bug in processing group names.

--- app/Libraries/CafeVariome/Core/DataPipeLine/Input/DataInput.php
+++ app/Libraries/CafeVariome/Core/DataPipeLine/Input/DataInput.php
@@ -262,6 +262,8 @@ abstract class DataInput extends DataPipeLine
 	protected function associateGroupWithAttributes(string $group_name, array $attribute_ids)
 	{
 		$group_name = trim(strtolower(preg_replace('/\s+/', '_', $group_name))); // replace spaces with underline
+		$group_name = $this->sanitiseString($group_name);
+
 		$group = $this->groups[$group_name];
 		$group_attributes = $group['attribute_ids'];
 		$attribute_ids_to_add = [];
