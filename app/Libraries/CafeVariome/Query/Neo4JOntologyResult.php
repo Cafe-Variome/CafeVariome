fbd91d62569a3ea48b1e2855e9fd575088c83cec
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-08 16:53:20 +0100

    Replaced models with data adapter classes.

--- app/Libraries/CafeVariome/Query/Neo4JOntologyResult.php
+++ app/Libraries/CafeVariome/Query/Neo4JOntologyResult.php
@@ -8,8 +8,8 @@
  *
  */
 
-use App\Models\Attribute;
-use App\Models\Ontology;
+use App\Libraries\CafeVariome\Factory\AttributeAdapterFactory;
+use App\Libraries\CafeVariome\Factory\OntologyAdapterFactory;
 use App\Libraries\CafeVariome\Entities\Source;
 
 class Neo4JOntologyResult extends AbstractResult
@@ -18,13 +18,13 @@ class Neo4JOntologyResult extends AbstractResult
 	public function Extract(array $ids, string $attribute, Source $source): array
 	{
 		$neo4jClient = $this->getNeo4JInstance();
-		$attributeModel = new Attribute();
-		$ontologyModel = new Ontology();
+		$attributeAdapter = (new AttributeAdapterFactory())->GetInstance();
+		$ontologyAdapter = (new OntologyAdapterFactory())->GetInstance();
 
 		$source_id = $source->getID();
 		$sourceUID = $source->uid;
-		$attributeId = $attributeModel->getAttributeIdByNameAndSourceId($attribute, $source_id);
-		$ontologyAssociations = $attributeModel->getOntologyAssociationsByAttributeId($attributeId);
+		$attributeId = $attributeAdapter->ReadIdByNameAndSourceId($attribute, $source_id);
+		$ontologyAssociations = $ontologyAdapter->ReadOntologyAssociationsByAttributeId($attributeId);
 
 		$idsString = '';
 		for($c = 0; $c < count($ids); $c++)
@@ -39,12 +39,14 @@ class Neo4JOntologyResult extends AbstractResult
 
 		$results = [];
 
-		foreach ($ontologyAssociations as $ontologyAssociation) {
-			$ontologyId = $ontologyAssociation['ontology_id'];
-			$ontology = $ontologyModel->getOntology($ontologyId);
-			$ontologyNodeKey = $ontology['node_key'];
-			$ontologyNodeType = $ontology['node_type'];
-			$ontologyTermName = $ontology['term_name'];
+		foreach ($ontologyAssociations as $ontologyAssociation)
+		{
+			$ontologyId = $ontologyAssociation->ontology_id;
+			$ontology = $ontologyAdapter->Read($ontologyId);
+
+			$ontologyNodeKey = $ontology->node_key;
+			$ontologyNodeType = $ontology->node_type;
+			$ontologyTermName = $ontology->term_name;
 
 			$neo4jQuery = "MATCH (s:Subject)-[]-(n:$ontologyNodeType) where (s.subjectid IN [$idsString] AND s.source_id='$source_id' AND s.uid='$sourceUID') RETURN distinct s.subjectid as subjectid, n.$ontologyNodeKey as node_key";
 
