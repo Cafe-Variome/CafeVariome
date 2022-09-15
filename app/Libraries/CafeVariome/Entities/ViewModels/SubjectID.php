5ad682aeb38a06f78510c2db2c1a14c80f111c7a
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-15 16:44:33 +0100

    Created SubjectID view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/SubjectID.php
@@ -0,0 +1,13 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+/**
+ * SubjectID.php
+ * Created 15/09/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class SubjectID extends BaseViewModel
+{
+
+}
