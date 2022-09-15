ed4b674055b4c80cbabda877ff4c985f2e693ba1
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-15 16:44:24 +0100

    Created ValueAttributeID view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/ValueAttributeID.php
@@ -0,0 +1,13 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+/**
+ * ValueAttributeID.php
+ * Created 15/09/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class ValueAttributeID extends BaseViewModel
+{
+	public int $attribute_id;
+}
