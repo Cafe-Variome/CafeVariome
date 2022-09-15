c2b73d5f881f6bf9ca265da7ed2734c4cea42a46
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-15 16:44:41 +0100

    Created AttributeID view model class.

---
+++ app/Libraries/CafeVariome/Entities/ViewModels/AttributeID.php
@@ -0,0 +1,13 @@
+<?php namespace App\Libraries\CafeVariome\Entities\ViewModels;
+
+/**
+ * AttributeID.php
+ * Created 15/09/2022
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+class AttributeID extends BaseViewModel
+{
+
+}
