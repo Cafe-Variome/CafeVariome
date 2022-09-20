461453bd4b5b4c4c1f527a6dfd20b379ef00e162
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-20 14:19:54 +0100

    Display policy of discovery group.

--- app/Views/Discover/SelectNetwork.php
+++ app/Views/Discover/SelectNetwork.php
@@ -29,10 +29,12 @@
 			<?php if($i == 0 ): ?>
 				<div class="row">
 			<?php elseif(($i) % 4 == 0): ?>
-				</div><div class="row mt-3">
+				</div><hr><div class="row mt-3">
 			<?php endif ?>
 			<div class="col-3">
-				<a class="btn btn-lg btn-info font-weight-bolder" href="<?= base_url($controllerName . '/QueryBuilder/' . $networks[$i]->network_id ) ?>"><i class="fas fa-fw fa-network-wired"></i> <?= $networks[$i]->network_name ?></a>
+				<a class="btn btn-lg btn-info font-weight-bolder w-100" href="<?= base_url($controllerName . '/QueryBuilder/' . $networks[$i]->network_id ) ?>"><i class="fas fa-fw fa-network-wired"></i> <?= $networks[$i]->network_name ?></a>
+				<br>
+				<span class="text-info">Policy: </span><span class="text-muted"><?= $networks[$i]->policy ?></span>
 			</div>
 			<?php if($i + 1 == count($networks)): ?>
 				</div>
