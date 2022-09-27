f81c857a0d3ec05bbc9cb379ab6e870b36b8a779
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-09-27 15:50:52 +0100

    Created InputValidation filter class.

---
+++ app/Filters/InputValidation.php
@@ -0,0 +1,26 @@
+<?php namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class InputValidation implements \CodeIgniter\Filters\FilterInterface
+{
+	public function before(RequestInterface $request, $arguments = null)
+	{
+		$post = $request->getVar();
+		foreach ($post as $field => &$value)
+		{
+			if (!is_array($value))
+			{
+				$value = htmlspecialchars_decode($value, ENT_QUOTES | ENT_SUBSTITUTE);
+				$value = htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+			}
+		}
+		$request->setGlobal('request', $post);
+	}
+
+	public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+	{
+		// TODO: Implement after() method.
+	}
+}
