a161b02826f280d671e87d58325dfdc866f89056
Vatsalya Maddi <vatsalyarini@gmail.com>
2022-09-27 12:06:42 +0100

    added support for privacy policy

--- app/Views/layout/master.php
+++ app/Views/layout/master.php
@@ -1,43 +1,44 @@
 <?php
 
 /**
-    *@author Mehdi Mehtarizadeh
-    *
-    *This is the master layout for all pages.
-*/
+ *@author Mehdi Mehtarizadeh
+ *
+ *This is the master layout for all pages.
+ */
 
 ?>
 <!doctype html>
 <html class="h-100">
+
 <head>
     <title><?php echo $site_title ?> | <?php echo $title ?></title>
 
     <meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="keywords" content="<?php echo $meta_keywords ?>" />
-	<meta name="author" content="<?php echo $meta_author ?>" />
-	<meta name="description" content="<?php echo $meta_description ?>" />
+    <meta name="keywords" content="<?php echo $meta_keywords ?>" />
+    <meta name="author" content="<?php echo $meta_author ?>" />
+    <meta name="description" content="<?php echo $meta_description ?>" />
 
-    <link rel="stylesheet" href="<?php echo base_url(VENDOR . "twbs/bootstrap/dist/css/bootstrap.css");?>" />
-    <link rel="stylesheet" href="<?php echo base_url(CSS . "site.css");?>" />
+    <link rel="stylesheet" href="<?php echo base_url(VENDOR . "twbs/bootstrap/dist/css/bootstrap.css"); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url(CSS . "site.css"); ?>" />
     <link rel="stylesheet" href="<?php echo base_url(VENDOR . "components/font-awesome/css/fontawesome.css"); ?>" />
     <link rel="stylesheet" href="<?php echo base_url(VENDOR . "components/font-awesome/css/brands.css"); ?>" />
     <link rel="stylesheet" href="<?php echo base_url(VENDOR . "components/font-awesome/css/solid.css"); ?>" />
-    <link rel="stylesheet" href="<?php echo base_url(VENDOR . "select2/select2/dist/css/select2.css");?>" />
+    <link rel="stylesheet" href="<?php echo base_url(VENDOR . "select2/select2/dist/css/select2.css"); ?>" />
 
 
     <!-- extra CSS-->
-    <?php foreach($css as $c):?>
-    <link rel="stylesheet" href="<?php echo base_url($c) ?>">
-    <?php endforeach;?>
+    <?php foreach ($css as $c) : ?>
+        <link rel="stylesheet" href="<?php echo base_url($c) ?>">
+    <?php endforeach; ?>
 
     <!-- favicon and touch icons -->
-    <link rel="shortcut icon" href="<?php echo base_url(IMAGES.'logos/favicon.ico');?>" />
+    <link rel="shortcut icon" href="<?php echo base_url(IMAGES . 'logos/favicon.ico'); ?>" />
 
-	<script type="text/javascript" src="<?= base_url('UserInterfaceAPI/GetUIConstants') ?>"></script>
+    <script type="text/javascript" src="<?= base_url('UserInterfaceAPI/GetUIConstants') ?>"></script>
 
-    <script src="<?php echo base_url(JS."jquery-3.6.0.min.js");?>"></script>
+    <script src="<?php echo base_url(JS . "jquery-3.6.0.min.js"); ?>"></script>
 
 </head>
 
@@ -55,27 +56,38 @@
 
     <footer id="footer" class="footer <?= ($stickyFooter) ? 'footer-sticky' : '' ?> mt-auto py-3">
         <div class="container">
-			<div class="row">
-				<div class="col text-center">
-					<span>
-						Powered by <a target="_blank" href="https://www.cafevariome.org/">Café Variome </a>
-						<br> By <a target="_blank" href="https://le.ac.uk/health-data-research">Bioinformatics and Health Data Science Group</a>
-						<br> Copyright &copy; <?= date("Y") ?>, <a target="_blank" href="https://le.ac.uk">University of Leicester</a>
-					</span>
-				</div>
-			</div>
-		</div>
+            <div class="row">
+                <div class="col text-center">
+                    <span>
+                        Powered by <a target="_blank" href="https://www.cafevariome.org/">Café Variome </a>
+                        <!-- If user is logged in, show the privacy policy in footer. Else this modal is shown in the top nav bar to be easily accessible (i.e., when user is not logged in) -->
+                        <!-- Aware of the bug that user has to click on Cafe Variome logo in this page and then be shown privacy policy. Something to do with routing ... ex: /home/index/PrivacyPolicy.pdf does not work but /home/PrivacyPolicy.pdf works -->
+                        <?php if ($loggedIn) : ?>
+                            | <a class="btn btn-sm btn-secondary" data-toggle="modal" data-target="#privacyPolicyModal">Privacy Policy</a>
+                        <?php endif; ?>
+                        <br> By <a target="_blank" href="https://le.ac.uk/health-data-research">Bioinformatics and Health Data Science Group</a>
+                        <br> Copyright &copy; <?= date("Y") ?>, <a target="_blank" href="https://le.ac.uk">University of Leicester</a>
+                    </span>
+                </div>
+            </div>
+        </div>
     </footer>
 
-    <script src="<?php echo base_url(VENDOR."twbs/bootstrap/dist/js/bootstrap.bundle.js");?>"></script>
-    <script src="<?php echo base_url(VENDOR."select2/select2/dist/js/select2.js");?>"></script>
+    <script src="<?php echo base_url(VENDOR . "twbs/bootstrap/dist/js/bootstrap.bundle.js"); ?>"></script>
+    <script src="<?php echo base_url(VENDOR . "select2/select2/dist/js/select2.js"); ?>"></script>
     <script type="text/javascript">
         $('[data-toggle="tooltip"]').tooltip();
     </script>
 
     <!-- extra Java Script-->
-    <?php foreach($javascript as $js):?>
-    <script src="<?php echo (substr($js, 0, 4) != 'http' ? base_url($js) : $js)?>"></script>
-    <?php endforeach;?>
+    <?php foreach ($javascript as $js) : ?>
+        <script src="<?php echo (substr($js, 0, 4) != 'http' ? base_url($js) : $js) ?>"></script>
+    <?php endforeach; ?>
 </body>
+<script>
+    $(document).on('show.bs.modal', '.modal', function() {
+        $(this).appendTo('body');
+    });
+</script>
+
 </html>
