f8eac92126b7412a97893472417f4ca5dada0ba0
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2022-12-18 23:54:24 +0000

    Created AttributeMapping entity class.

---
+++ app/Libraries/CafeVariome/Entities/AttributeMapping.php
@@ -0,0 +1,16 @@
+<?php namespace App\Libraries\CafeVariome\Entities;
+
+/**
+ * AttributeMapping.php
+ * Created 15/12/2022
+ *
+ * This class extends Entity.
+ * @author Mehdi Mehtarizadeh
+ */
+
+class AttributeMapping extends Entity
+{
+	public string $name;
+
+	public int $attribute_id;
+}
