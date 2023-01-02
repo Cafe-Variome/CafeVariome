67ddca6774036ab8febac9d8686d7adda27d2fdc
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2023-01-02 23:38:01 +0000

    Created ValueMapping entity class.

---
+++ app/Libraries/CafeVariome/Entities/ValueMapping.php
@@ -0,0 +1,16 @@
+<?php namespace App\Libraries\CafeVariome\Entities;
+
+/**
+ * ValueMapping.php
+ * Created 15/12/2022
+ *
+ * This class extends Entity.
+ * @author Mehdi Mehtarizadeh
+ */
+
+class ValueMapping extends Entity
+{
+	public string $name;
+
+	public int $value_id;
+}
