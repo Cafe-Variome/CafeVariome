c4d87a390a99ead2738eaa167c64c1ee1ad1b483
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2023-02-09 11:29:11 +0000

    Created NetworkRequestAdapterFactory class.

---
+++ app/Libraries/CafeVariome/Factory/NetworkRequestAdapterFactory.php
@@ -0,0 +1,22 @@
+<?php namespace APP\Libraries\CafeVariome\Factory;
+
+/**
+ * NetworkRequestAdapterFactory.php
+ * Created 30/01/2023
+ *
+ * @author Mehdi Mehtarizadeh
+ */
+
+use App\Libraries\CafeVariome\Database\IAdapter;
+use App\Libraries\CafeVariome\Database\NetworkRequestAdapter;
+
+class NetworkRequestAdapterFactory extends AdapterFactory
+{
+    /**
+     * @inheritDoc
+     */
+    public function GetInstance(): IAdapter
+    {
+        return new NetworkRequestAdapter();
+    }
+}
