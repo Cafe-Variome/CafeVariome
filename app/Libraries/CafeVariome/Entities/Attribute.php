f37b6625db4da072e15fa22fbd4eaf9c410bd376
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2023-02-24 11:33:35 +0000

    Added comments.

--- app/Libraries/CafeVariome/Entities/Attribute.php
+++ app/Libraries/CafeVariome/Entities/Attribute.php
@@ -10,21 +10,49 @@
 
 class Attribute extends Entity
 {
+	/**
+	 * @var string name of attribute
+	 */
 	public string $name;
 
+	/**
+	 * @var string display name of attribute used in query interface
+	 */
 	public string $display_name;
 
+	/**
+	 * @var int source ID of the attribute
+	 */
 	public int $source_id;
 
+	/**
+	 * @var int type of attribute
+	 * @see app/Config/Constants.php for types
+	 */
 	public int $type;
 
+	/**
+	 * @var float minimum value of attribute if applicable
+	 */
 	public float $min;
 
+	/**
+	 * @var float maximum value of attribute if applicable
+	 */
 	public float $max;
 
+	/**
+	 * @var bool whether the attribute is shown in the query interface, not used
+	 */
 	public bool $show_in_interface;
 
+	/**
+	 * @var bool whether the attribute is shown in the query interface index
+	 */
 	public bool $include_in_interface_index;
 
+	/**
+	 * @var int where the attribute is stored
+	 */
 	public int $storage_location;
 }
