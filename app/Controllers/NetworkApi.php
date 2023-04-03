f8259e5603b273151e71cbb616a4cea79cdf68c0
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2023-04-03 14:57:49 +0100

    Updated comments.

--- app/Controllers/NetworkApi.php
+++ app/Controllers/NetworkApi.php
@@ -2,14 +2,13 @@
 
 /**
  * NetworkApi.php
- * 
+ *
  * Created : 01/10/2019
- * 
+ *
  * @author Gregory Warren
  * @author Mehdi Mehtarizadeh
- * 
- * This controller contains RESTful listeners for network operations. 
- * Most methods are ported from netauth.php in the previous version.
+ *
+ * This controller contains RESTful listeners for network operations.
  */
 
 use App\Libraries\CafeVariome\Factory\NetworkRequestAdapterFactory;
