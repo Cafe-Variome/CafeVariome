1634698f97bf2a42c5ee2c822c50fb01b5c03d39
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2023-04-14 12:05:10 +0100

    Added comments and fixed types.

--- app/Libraries/CafeVariome/Auth/LocalAuthenticator.php
+++ app/Libraries/CafeVariome/Auth/LocalAuthenticator.php
@@ -14,51 +14,62 @@ use App\Libraries\CafeVariome\Factory\UserAdapterFactory;
 use CodeIgniter\Session\Session;
 use Config\Services;
 
-/**
- * Name: LocalAuthenticator.php
- * Created: 31/05/2020
- * @author Mehdi Mehtarizadeh
- *
- */
-
-class LocalAuthenticator
+class LocalAuthenticator implements  IAuthenticator
 {
-	protected $session;
+	protected Session $session;
 
 	protected UserAdapter $userAdapter;
 
 	public function __construct()
 	{
-		$this->session =  \Config\Services::session();
+		$this->session =  Services::session();
 		$this->userAdapter = (new UserAdapterFactory())->GetInstance();
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function GetUserIdByEmail(string $email): ?int
 	{
 		return $this->userAdapter->ReadIdByEmail($email);
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function GetUserById(int $user_id): IEntity
 	{
 		return $this->userAdapter->Read($user_id);
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function GetUserId(): int
 	{
 		return $this->session->has('user_id') ? $this->session->get('user_id') : -1;
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function LoggedIn(): bool
 	{
 		return $this->session->has('user_id');
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function IsAdmin(): bool
 	{
 		return $this->session->has('is_admin') && $this->session->get('is_admin');
 	}
 
-	public function RecordSession(User $user)
+	/**
+	 * @inheritDoc
+	 */
+	public function RecordSession(User $user): void
 	{
 		$session_data = array(
 			'user_id'                   => $user->getID(),
@@ -70,16 +81,26 @@ class LocalAuthenticator
 		$this->session->set($session_data);
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function GetLogoutURL(): string
 	{
 		return base_url('Home/Index');
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function GetProfileEndpoint(): string
 	{
 		return base_url('Auth/Profile');
 	}
 
+	/**
+	 * @return bool
+	 * As this is a local authenticator it always returns true.
+	 */
 	public function Ping(): bool
 	{
 		return true;
