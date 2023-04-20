c227a23f1071288dc47da2d60acb51aee8c89718
Mehdi Mehtarizadeh <mm917@leicester.ac.uk>
2023-04-20 15:12:42 +0100

    Completed manual routing.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,7 +29,13 @@ $routes->setAutoRoute(false);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::Index');
+
+/**
+ * Cafe Variome Manual Routes
+ * @author Mehdi Mehtarizadeh
+ * Please do not change the routes below unless new methods/controllers are created.
+ */
 
 $routes->get('admin', 'Admin::Index');
 $routes->get('Admin', 'Admin::Index');
@@ -46,7 +52,12 @@ $routes->post('AjaxApi/ImportFromDirectory', 'AjaxApi::ImportFromDirectory');
 $routes->post('AjaxApi/ProcessFile', 'AjaxApi::ProcessFile');
 $routes->post('AjaxApi/ProcessFiles', 'AjaxApi::ProcessFiles');
 $routes->post('AjaxApi/GetPhenotypeAttributes/(:num)', 'AjaxApi::GetPhenotypeAttributes/$1');
-$routes->post('AjaxApi', 'AjaxApi::');
+$routes->post('AjaxApi/CreateUserInterfaceIndex', 'AjaxApi::CreateUserInterfaceIndex');
+$routes->post('AjaxApi/CountUploadedAndImportedFiles', 'AjaxApi::CountUploadedAndImportedFiles');
+$routes->post('AjaxApi/IndexDataToElasticsearch', 'AjaxApi::IndexDataToElasticsearch');
+$routes->post('AjaxApi/IndexDataToNeo4J', 'AjaxApi::IndexDataToNeo4J');
+$routes->post('AjaxApi/StartService', 'AjaxApi::StartService');
+$routes->post('AjaxApi/ShutdownService', 'AjaxApi::ShutdownService');
 
 $routes->get('Attribute', 'Attribute::Index');
 $routes->get('Attribute/Index', 'Attribute::Index');
@@ -67,9 +78,10 @@ $routes->post('AttributeMapping/Create/(:num)', 'AttributeMapping::Create/$1');
 $routes->get('AttributeMapping/Delete/(:num)', 'AttributeMapping::Delete/$1');
 $routes->post('AttributeMapping/Delete/(:num)', 'AttributeMapping::Delete/$1');
 
-$routes->get('Auth', 'Auth::');
+$routes->get('Auth', 'Auth::Index');
 $routes->get('Auth/Index', 'Auth::Index');
 $routes->get('Auth/Login', 'Auth::Login');
+$routes->get('auth/login', 'Auth::Login');
 $routes->post('Auth/Login', 'Auth::Login');
 $routes->get('Auth/Logout', 'Auth::Logout');
 
@@ -117,20 +129,195 @@ $routes->get('Discover/Index', 'Discover::Index');
 $routes->get('Discover/SelectNetwork', 'Discover::SelectNetwork');
 $routes->get('Discover/QueryBuilder/(:num)', 'Discover::QueryBuilder/$1');
 
-$routes->get('Home/Index', 'Home::index');
+$routes->get('DiscoveryGroup', 'DiscoveryGroup::Index');
+$routes->get('DiscoveryGroup/Index', 'DiscoveryGroup::Index');
+$routes->get('DiscoveryGroup/Create', 'DiscoveryGroup::Create');
+$routes->post('DiscoveryGroup/Create', 'DiscoveryGroup::Create');
+$routes->get('DiscoveryGroup/List', 'DiscoveryGroup::List');
+$routes->get('DiscoveryGroup/Update/(:num)', 'DiscoveryGroup::Update/$1');
+$routes->post('DiscoveryGroup/Update/(:num)', 'DiscoveryGroup::Update/$1');
+$routes->get('DiscoveryGroup/Delete/(:num)', 'DiscoveryGroup::Delete/$1');
+$routes->post('DiscoveryGroup/Delete/(:num)', 'DiscoveryGroup::Delete/$1');
+$routes->get('DiscoveryGroup/Details/(:num)', 'DiscoveryGroup::Details/$1');
 
+$routes->get('Home', 'Home::Index');
+$routes->get('home', 'Home::Index');
+$routes->get('Home/Index', 'Home::Index');
+$routes->get('home/index', 'Home::Index');
+$routes->get('home/index/(:num)', 'Home::Index/$1');
+$routes->get('Home/Index/(:num)', 'Home::Index/$1');
+
+$routes->get('Network', 'Network::Index');
+$routes->get('Network/Create', 'Network::Create');
+$routes->post('Network/Create', 'Network::Create');
+$routes->get('Network/List', 'Network::List');
+$routes->get('Network/Join', 'Network::Join');
+$routes->post('Network/Join', 'Network::Join');
+$routes->get('Network/Leave/(:num)', 'Network::Leave/$1');
+$routes->post('Network/Leave/(:num)', 'Network::Leave/$1');
+
+$routes->post('NetworkAPI/requestToJoinNetwork', 'NetworkAPI::requestToJoinNetwork');
+$routes->post('NetworkApi/requestToJoinNetwork', 'NetworkAPI::requestToJoinNetwork');
+
+$routes->get('NetworkRequest', 'NetworkRequest::Index');
+$routes->get('NetworkRequest/Index', 'NetworkRequest::Index');
+$routes->get('NetworkRequest/List', 'NetworkRequest::List');
+$routes->get('NetworkRequest/Accept/(:num)', 'NetworkRequest::Accept/$1');
+$routes->get('NetworkRequest/Reject/(:num)', 'NetworkRequest::Reject/$1');
+
+$routes->get('Ontology', 'Ontology::Index');
+$routes->get('Ontology/Index', 'Ontology::Index');
+$routes->get('Ontology/Create', 'Ontology::Create');
+$routes->post('Ontology/Create', 'Ontology::Create');
+$routes->get('Ontology/List', 'Ontology::List');
+$routes->get('Ontology/Update/(:num)', 'Ontology::Update/$1');
+$routes->post('Ontology/Update/(:num)', 'Ontology::Update/$1');
+$routes->get('Ontology/Delete/(:num)', 'Ontology::Delete/$1');
+$routes->post('Ontology/Delete/(:num)', 'Ontology::Delete/$1');
+$routes->get('Ontology/Details/(:num)', 'Ontology::Details/$1');
+
+$routes->get('OntologyPrefix', 'OntologyPrefix::Index');
+$routes->get('OntologyPrefix/Index', 'OntologyPrefix::Index');
+$routes->get('OntologyPrefix/List/(:num)', 'OntologyPrefix::List/$1');
+$routes->get('OntologyPrefix/Create/(:num)', 'OntologyPrefix::Create/$1');
+$routes->post('OntologyPrefix/Create/(:num)', 'OntologyPrefix::Create/$1');
+$routes->get('OntologyPrefix/Update/(:num)', 'OntologyPrefix::Update/$1');
+$routes->post('OntologyPrefix/Update/(:num)', 'OntologyPrefix::Update/$1');
+$routes->get('OntologyPrefix/Delete/(:num)', 'OntologyPrefix::Delete/$1');
+$routes->post('OntologyPrefix/Delete/(:num)', 'OntologyPrefix::Delete/$1');
+
+$routes->get('OntologyRelationship', 'OntologyRelationship::Index');
+$routes->get('OntologyRelationship/Index', 'OntologyRelationship::Index');
+$routes->get('OntologyRelationship/List/(:num)', 'OntologyRelationship::List/$1');
+$routes->get('OntologyRelationship/Create/(:num)', 'OntologyRelationship::Create/$1');
+$routes->post('OntologyRelationship/Create/(:num)', 'OntologyRelationship::Create/$1');
+$routes->get('OntologyRelationship/Update/(:num)', 'OntologyRelationship::Update/$1');
+$routes->post('OntologyRelationship/Update/(:num)', 'OntologyRelationship::Update/$1');
+$routes->get('OntologyRelationship/Delete/(:num)', 'OntologyRelationship::Delete/$1');
+$routes->post('OntologyRelationship/Delete/(:num)', 'OntologyRelationship::Delete/$1');
+
+$routes->get('Page', 'Page::Index');
+$routes->get('Page/Index', 'Page::Index');
+$routes->get('Page/List', 'Page::List');
+$routes->get('Page/Create', 'Page::Create');
+$routes->post('Page/Create', 'Page::Create');
+$routes->get('Page/Update/(:num)', 'Page::Update/$1');
+$routes->post('Page/Update/(:num)', 'Page::Update/$1');
+$routes->get('Page/Activate/(:num)', 'Page::Activate/$1');
+$routes->get('Page/Deactivate/(:num)', 'Page::Deactivate/$1');
+$routes->get('Page/Delete/(:num)', 'Page::Delete/$1');
+$routes->post('Page/Delete/(:num)', 'Page::Delete/$1');
+
+$routes->get('Pipeline', 'Pipeline::Index');
+$routes->get('Pipeline/Index', 'Pipeline::Index');
+$routes->get('Pipeline/List', 'Pipeline::List');
+$routes->get('Pipeline/Delete/(:num)', 'Pipeline::Delete/$1');
+$routes->post('Pipeline/Delete/(:num)', 'Pipeline::Delete/$1');
+$routes->get('Pipeline/Create', 'Pipeline::Create');
+$routes->post('Pipeline/Create', 'Pipeline::Create');
+$routes->get('Pipeline/Details/(:num)', 'Pipeline::Details/$1');
+$routes->get('Pipeline/Update/(:num)', 'Pipeline::Update/$1');
+$routes->post('Pipeline/Update/(:num)', 'Pipeline::Update/$1');
+
+$routes->get('ProxyServer', 'ProxyServer::Index');
+$routes->get('ProxyServer/Index', 'ProxyServer::Index');
+$routes->get('ProxyServer/List', 'ProxyServer::List');
+$routes->get('ProxyServer/Create', 'ProxyServer::Create');
+$routes->post('ProxyServer/Create', 'ProxyServer::Create');
+$routes->get('ProxyServer/Update/(:num)', 'ProxyServer::Update/$1');
+$routes->post('ProxyServer/Update/(:num)', 'ProxyServer::Update/$1');
+$routes->get('ProxyServer/Details/(:num)', 'ProxyServer::Details/$1');
+$routes->get('ProxyServer/Delete/(:num)', 'ProxyServer::Delete/$1');
+$routes->post('ProxyServer/Delete/(:num)', 'ProxyServer::Delete/$1');
+
+$routes->post('QueryApi/Query', 'QueryApi::Query');
+$routes->post('QueryAPI/Query', 'QueryApi::Query');
+$routes->post('QueryApi/getJSONDataModificationTime', 'QueryApi::getJSONDataModificationTime');
+$routes->post('QueryApi/getEAVJSON', 'QueryApi::getEAVJSON');
+$routes->post('QueryApi/getHPOJSON', 'QueryApi::getHPOJSON');
+
+$routes->get('Server', 'Server::Index');
+$routes->get('Server/Index', 'Server::Index');
+$routes->get('Server/List', 'Server::List');
+$routes->get('Server/Create', 'Server::Create');
+$routes->post('Server/Create', 'Server::Create');
+$routes->get('Server/Update/(:num)', 'Server::Update/$1');
+$routes->post('Server/Update/(:num)', 'Server::Update/$1');
+$routes->get('Server/Details/(:num)', 'Server::Details/$1');
+$routes->get('Server/Delete/(:num)', 'Server::Delete/$1');
+$routes->post('Server/Delete/(:num)', 'Server::Delete/$1');
+
+$routes->get('ServiceApi/PollTasks', 'ServiceAPI::PollTasks');
+$routes->get('ServiceAPI/PollTasks', 'ServiceAPI::PollTasks');
+$routes->post('ServiceApi/PollTask', 'ServiceAPI::PollTask');
+$routes->post('ServiceAPI/PollTask', 'ServiceAPI::PollTask');
+
+$routes->get('Setting/Discovery', 'Setting::Discovery');
+$routes->post('Setting/Discovery', 'Setting::Discovery');
+$routes->get('Setting/Endpoint', 'Setting::Endpoint');
+$routes->post('Setting/Endpoint', 'Setting::Endpoint');
+$routes->get('Setting/Main', 'Setting::Main');
+$routes->post('Setting/Main', 'Setting::Main');
+$routes->get('Setting/Elasticsearch', 'Setting::Elasticsearch');
+$routes->post('Setting/Elasticsearch', 'Setting::Elasticsearch');
+$routes->get('Setting/Neo4J', 'Setting::Neo4J');
+$routes->post('Setting/Neo4J', 'Setting::Neo4J');
+
+$routes->get('SingleSignOnProvider', 'SingleSignOnProvider::Index');
+$routes->get('SingleSignOnProvider/Index', 'SingleSignOnProvider::Index');
+$routes->get('SingleSignOnProvider/List', 'SingleSignOnProvider::List');
+$routes->get('SingleSignOnProvider/Create', 'SingleSignOnProvider::Create');
+$routes->post('SingleSignOnProvider/Create', 'SingleSignOnProvider::Create');
+$routes->get('SingleSignOnProvider/Update/(:num)', 'SingleSignOnProvider::Update/$1');
+$routes->post('SingleSignOnProvider/Update/(:num)', 'SingleSignOnProvider::Update/$1');
+$routes->get('SingleSignOnProvider/Details/(:num)', 'SingleSignOnProvider::Details/$1');
+$routes->get('SingleSignOnProvider/Delete/(:num)', 'SingleSignOnProvider::Delete/$1');
+$routes->post('SingleSignOnProvider/Delete/(:num)', 'SingleSignOnProvider::Delete/$1');
 
 $routes->get('Source', 'Source::Index');
 $routes->get('Source/Index', 'Source::Index');
 $routes->get('Source/List', 'Source::List');
-//$routes->get('Source/', 'Source::');
-//
-//
-//$routes->get('', '');
+$routes->get('Source/Create', 'Source::Create');
+$routes->post('Source/Create', 'Source::Create');
+$routes->get('Source/Update/(:num)', 'Source::Update/$1');
+$routes->post('Source/Update/(:num)', 'Source::Update/$1');
+$routes->get('Source/Delete/(:num)', 'Source::Delete/$1');
+$routes->post('Source/Delete/(:num)', 'Source::Delete/$1');
+$routes->get('Source/Elasticsearch/(:num)', 'Source::Elasticsearch/$1');
+$routes->get('Source/Neo4J/(:num)', 'Source::Neo4J/$1');
+$routes->get('Source/UserInterface/(:num)', 'Source::UserInterface/$1');
+
+$routes->cli('Task/Start/(:num)', 'Task::Start/$1');
+$routes->cli('Task/CreateBatchTasksForDataFiles/(:any)', 'Task::CreateBatchTasksForDataFiles/$1');
+$routes->cli('Task/StartService', 'Task::StartService');
+
+$routes->get('User', 'User::Index');
+$routes->get('User/Index', 'User::Index');
+$routes->get('User/List', 'User::List');
+$routes->get('User/Create', 'User::Create');
+$routes->post('User/Create', 'User::Create');
+$routes->get('User/Update/(:num)', 'User::Update/$1');
+$routes->post('User/Update/(:num)', 'User::Update/$1');
+$routes->get('User/Details/(:num)', 'User::Details/$1');
+$routes->get('User/Delete/(:num)', 'User::Delete/$1');
+$routes->post('User/Delete/(:num)', 'User::Delete/$1');
 
 $routes->get('UserInterfaceAPI/GetUIConstants', 'UserInterfaceAPI::GetUIConstants');
 
+$routes->get('Value', 'Value::Index');
+$routes->get('Value/Index', 'Value::Index');
+$routes->get('Value/List/(:num)', 'Value::List/$1');
+$routes->get('Value/Details/(:num)', 'Value::Details/$1');
+$routes->get('Value/Update/(:num)', 'Value::Update/$1');
+$routes->post('Value/Update/(:num)', 'Value::Update/$1');
 
+$routes->get('ValueMapping', 'ValueMapping::Index');
+$routes->get('ValueMapping/Index', 'ValueMapping::Index');
+$routes->get('ValueMapping/List/(:num)', 'ValueMapping::List/$1');
+$routes->get('ValueMapping/Create/(:num)', 'ValueMapping::Create/$1');
+$routes->post('ValueMapping/Create/(:num)', 'ValueMapping::Create/$1');
+$routes->get('ValueMapping/Delete/(:num)', 'ValueMapping::Delete/$1');
+$routes->post('ValueMapping/Delete/(:num)', 'ValueMapping::Delete/$1');
 
 /*
  * --------------------------------------------------------------------
