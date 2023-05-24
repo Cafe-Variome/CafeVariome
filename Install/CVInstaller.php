c0079f020807239880d992229168154f471980b4
Farid Yavari Dizjikan <fyd1980@gmail.com>
2023-05-24 09:23:18 +0100

    CVInstaller is updated due to creating docker container

--- Install/CVInstaller.php
+++ Install/CVInstaller.php
@@ -40,80 +40,80 @@ class CVInstaller
 	   // Location of the framework bootstrap file.
 	   require rtrim($paths->systemDirectory, '/ ') . '/bootstrap.php';
 
-	   echo "Creating database tables ... \n";
+	//    echo "Creating database tables ... \n";
 
-		$lines = file('../.env');
+	// 	$lines = file('../.env');
 
-		$db = [];
+	// 	$db = [];
 
 
-		foreach ($lines as $line)
-		{
-			if (str_starts_with($line, 'database.default.hostname'))
-			{
-				$db['hostname'] = trim(explode('=', $line)[1]);
-			}
-			else if(str_starts_with($line, 'database.default.database'))
-			{
-				$db['database'] = trim(explode('=', $line)[1]);
-			}
-			else if(str_starts_with($line, 'database.default.username'))
-			{
-				$db['username'] = trim(explode('=', $line)[1]);
-			}
-			else if(str_starts_with($line, 'database.default.password'))
-			{
-				$db['password'] = trim(explode('=', $line)[1]);
-			}
-			else if(str_starts_with($line, 'database.default.DBDriver'))
-			{
-				$db['DBDriver'] = trim(explode('=', $line)[1]);
-			}
+	// 	foreach ($lines as $line)
+	// 	{
+	// 		if (str_starts_with($line, 'database.default.hostname'))
+	// 		{
+	// 			$db['hostname'] = trim(explode('=', $line)[1]);
+	// 		}
+	// 		else if(str_starts_with($line, 'database.default.database'))
+	// 		{
+	// 			$db['database'] = trim(explode('=', $line)[1]);
+	// 		}
+	// 		else if(str_starts_with($line, 'database.default.username'))
+	// 		{
+	// 			$db['username'] = trim(explode('=', $line)[1]);
+	// 		}
+	// 		else if(str_starts_with($line, 'database.default.password'))
+	// 		{
+	// 			$db['password'] = trim(explode('=', $line)[1]);
+	// 		}
+	// 		else if(str_starts_with($line, 'database.default.DBDriver'))
+	// 		{
+	// 			$db['DBDriver'] = trim(explode('=', $line)[1]);
+	// 		}
 
-		}
+	// 	}
 
-	   // Name of the file
-	   $filename = 'MySql/cafevariome.sql';
-	   // MySQL host
-	   $mysql_host = $db['hostname'];
-	   // MySQL username
-	   $mysql_username = $db['username'];
-	   // MySQL password
-	   $mysql_password = $db['password'];
-	   // Database name
-	   $mysql_database = $db['database'];
+	//    // Name of the file
+	//    $filename = 'MySql/cafevariome.sql';
+	//    // MySQL host
+	//    $mysql_host = $db['hostname'];
+	//    // MySQL username
+	//    $mysql_username = $db['username'];
+	//    // MySQL password
+	//    $mysql_password = $db['password'];
+	//    // Database name
+	//    $mysql_database = $db['database'];
 
-	   // Connect to MySQL server
-	   $con = @new \mysqli($mysql_host, $mysql_username, $mysql_password, $mysql_database);
+	//    // Connect to MySQL server
+	//    $con = @new \mysqli($mysql_host, $mysql_username, $mysql_password, $mysql_database);
 
-	   // Check connection
-	   if ($con->connect_errno) {
-		   echo "Failed to connect to MySQL: " . $con->connect_errno;
-		   echo "<br/>Error: " . $con->connect_error;
-	   }
+	//    // Check connection
+	//    if ($con->connect_errno) {
+	// 	   echo "Failed to connect to MySQL: " . $con->connect_errno;
+	// 	   echo "<br/>Error: " . $con->connect_error;
+	//    }
 
-	   // Temporary variable, used to store current query
-	   $templine = '';
-	   // Read in entire file
-	   $lines = file($filename);
-	   // Loop through each line
-	   foreach ($lines as $line) {
-		   // Skip it if it's a comment
-		   if (substr($line, 0, 2) == '--' || $line == '')
-			   continue;
+	//    // Temporary variable, used to store current query
+	//    $templine = '';
+	//    // Read in entire file
+	//    $lines = file($filename);
+	//    // Loop through each line
+	//    foreach ($lines as $line) {
+	// 	   // Skip it if it's a comment
+	// 	   if (substr($line, 0, 2) == '--' || $line == '')
+	// 		   continue;
 
-		   // Add this line to the current segment
-		   $templine .= $line;
-		   // If it has a semicolon at the end, it's the end of the query
-		   if (substr(trim($line), -1, 1) == ';') {
-			   // Perform the query
-			   $con->query($templine);
-			   // Reset temp variable to empty
-			   $templine = '';
-		   }
-	   }
+	// 	   // Add this line to the current segment
+	// 	   $templine .= $line;
+	// 	   // If it has a semicolon at the end, it's the end of the query
+	// 	   if (substr(trim($line), -1, 1) == ';') {
+	// 		   // Perform the query
+	// 		   $con->query($templine);
+	// 		   // Reset temp variable to empty
+	// 		   $templine = '';
+	// 	   }
+	//    }
 
-	   echo "Tables imported successfully \n";
+	//    echo "Tables imported successfully \n";
 
 	   // print("Please enter your installation key:");
 	   $installation_key = readline("Please enter your installation key:");
@@ -137,171 +137,171 @@ class CVInstaller
 	   $con->close();
 	}
 
-	public static function Deploy(string $base_url, string $installation_key, string $php_bin_path, string $apache_config_file, string $deployment_directory, array $database_info)
-	{
-		// Valid PHP Version?
-		$minPHPVersion = '8.0';
-		if (phpversion() < $minPHPVersion) {
-			die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . phpversion());
-		}
+	// public static function Deploy(string $base_url, string $installation_key, string $php_bin_path, string $apache_config_file, string $deployment_directory, array $database_info)
+	// {
+	// 	// Valid PHP Version?
+	// 	$minPHPVersion = '8.0';
+	// 	if (phpversion() < $minPHPVersion) {
+	// 		die("Your PHP version must be {$minPHPVersion} or higher to run CodeIgniter. Current version: " . phpversion());
+	// 	}
 
-		unset($minPHPVersion);
+	// 	unset($minPHPVersion);
 
-		// Ensure the current directory is pointing to the front controller's directory
-		chdir(__DIR__);
-		$appPath = '../';
+	// 	// Ensure the current directory is pointing to the front controller's directory
+	// 	chdir(__DIR__);
+	// 	$appPath = '../';
 
-		// Create the database and user
-		$filename = 'MySql/cafevariome.sql';
+	// 	// Create the database and user
+	// 	$filename = 'MySql/cafevariome.sql';
 
-		$dbHost = $database_info['host'];
-		$dbRootUser = $database_info['root_user'];
-		$dbRootPassword = $database_info['root_password'];
-		$dbName = $database_info['db_name'];
+	// 	$dbHost = $database_info['host'];
+	// 	$dbRootUser = $database_info['root_user'];
+	// 	$dbRootPassword = $database_info['root_password'];
+	// 	$dbName = $database_info['db_name'];
 
-		$dbUsername = $database_info['username'];
-		$dbPassword = md5(uniqid());
+	// 	$dbUsername = $database_info['username'];
+	// 	$dbPassword = md5(uniqid());
 
-		$con = @new \mysqli($dbHost, $dbRootUser, $dbRootPassword);
-		// Check connection
-		if ($con->connect_errno) {
-			echo "Failed to connect to MySQL as a privileged user: " . $con->connect_errno;
-			echo "<br/>Error: " . $con->connect_error;
-		}
+	// 	$con = @new \mysqli($dbHost, $dbRootUser, $dbRootPassword);
+	// 	// Check connection
+	// 	if ($con->connect_errno) {
+	// 		echo "Failed to connect to MySQL as a privileged user: " . $con->connect_errno;
+	// 		echo "<br/>Error: " . $con->connect_error;
+	// 	}
 
-		$con->query("Create database $dbName;");
-		$con->query("Create user $dbUsername identified by '$dbPassword';");
-		$con->query("Grant all privileges on $dbName.* to '$dbUsername';");
+	// 	$con->query("Create database $dbName;");
+	// 	$con->query("Create user $dbUsername identified by '$dbPassword';");
+	// 	$con->query("Grant all privileges on $dbName.* to '$dbUsername';");
 
-		$con->close();
+	// 	$con->close();
 
-		$con = @new \mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+	// 	$con = @new \mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-		// Check connection
-		if ($con->connect_errno) {
-			echo "Failed to connect to MySQL: " . $con->connect_errno;
-			echo "<br/>Error: " . $con->connect_error;
-		}
-		else{
-			// Temporary variable, used to store current query
-			$templine = '';
-			// Read in entire file
-			$lines = file($filename);
-			// Loop through each line
-			foreach ($lines as $line) {
-				// Skip it if it's a comment
-				if (substr($line, 0, 2) == '--' || $line == '')
-					continue;
+	// 	// Check connection
+	// 	if ($con->connect_errno) {
+	// 		echo "Failed to connect to MySQL: " . $con->connect_errno;
+	// 		echo "<br/>Error: " . $con->connect_error;
+	// 	}
+	// 	else{
+	// 		// Temporary variable, used to store current query
+	// 		$templine = '';
+	// 		// Read in entire file
+	// 		$lines = file($filename);
+	// 		// Loop through each line
+	// 		foreach ($lines as $line) {
+	// 			// Skip it if it's a comment
+	// 			if (substr($line, 0, 2) == '--' || $line == '')
+	// 				continue;
 
-				// Add this line to the current segment
-				$templine .= $line;
-				// If it has a semicolon at the end, it's the end of the query
-				if (substr(trim($line), -1, 1) == ';') {
-					// Perform the query
-					$con->query($templine);
-					// Reset temp variable to empty
-					$templine = '';
-				}
-			}
-			$con->query("Update settings set value = '$installation_key' where setting_key = 'installation_key';");
+	// 			// Add this line to the current segment
+	// 			$templine .= $line;
+	// 			// If it has a semicolon at the end, it's the end of the query
+	// 			if (substr(trim($line), -1, 1) == ';') {
+	// 				// Perform the query
+	// 				$con->query($templine);
+	// 				// Reset temp variable to empty
+	// 				$templine = '';
+	// 			}
+	// 		}
+	// 		$con->query("Update settings set value = '$installation_key' where setting_key = 'installation_key';");
 
-			echo 'Database imported successfully.';
-		}
+	// 		echo 'Database imported successfully.';
+	// 	}
 
-		$con->close();
+	// 	$con->close();
 
-		$envFile = $appPath . '.env';
-		$envFileHandle = fopen($envFile, 'a+');
-		if ($envFileHandle !== false)
-		{
-			$envData = "CI_ENVIRONMENT = development" . PHP_EOL;
-			$envData .= "app.baseURL = '$base_url'" . PHP_EOL;
-			$envData .= "app.baseURL = '$base_url'" . PHP_EOL;
-			$envData .= "database.default.hostname = $dbHost" . PHP_EOL;
-			$envData .= "database.default.database = $dbName" . PHP_EOL;
-			$envData .= "database.default.username = $dbUsername" . PHP_EOL;
-			$envData .= "database.default.password =  $dbPassword" . PHP_EOL;
-			$envData .= "database.default.DBDriver = MySQLi" . PHP_EOL;
-			$envData .= "#----------------------------------" . PHP_EOL;
-			$envData .= "PHP_BIN_PATH = $php_bin_path" . PHP_EOL;
-			$envData .= "EAV_BATCH_SIZE = 30000" . PHP_EOL;
-			$envData .= "NEO4J_BATCH_SIZE = 30000" . PHP_EOL;
-			$envData .= "SPREADSHEET_BATCH_SIZE = 1000" . PHP_EOL;
-			$envData .= "ELASTICSERACH_AGGREGATE_SIZE = 100000" . PHP_EOL;
-			$envData .= "ELASTICSERACH_EXTRACT_AGGREGATE_SIZE = 10000" . PHP_EOL;
+	// 	$envFile = $appPath . '.env';
+	// 	$envFileHandle = fopen($envFile, 'a+');
+	// 	if ($envFileHandle !== false)
+	// 	{
+	// 		$envData = "CI_ENVIRONMENT = development" . PHP_EOL;
+	// 		$envData .= "app.baseURL = '$base_url'" . PHP_EOL;
+	// 		$envData .= "app.baseURL = '$base_url'" . PHP_EOL;
+	// 		$envData .= "database.default.hostname = $dbHost" . PHP_EOL;
+	// 		$envData .= "database.default.database = $dbName" . PHP_EOL;
+	// 		$envData .= "database.default.username = $dbUsername" . PHP_EOL;
+	// 		$envData .= "database.default.password =  $dbPassword" . PHP_EOL;
+	// 		$envData .= "database.default.DBDriver = MySQLi" . PHP_EOL;
+	// 		$envData .= "#----------------------------------" . PHP_EOL;
+	// 		$envData .= "PHP_BIN_PATH = $php_bin_path" . PHP_EOL;
+	// 		$envData .= "EAV_BATCH_SIZE = 30000" . PHP_EOL;
+	// 		$envData .= "NEO4J_BATCH_SIZE = 30000" . PHP_EOL;
+	// 		$envData .= "SPREADSHEET_BATCH_SIZE = 1000" . PHP_EOL;
+	// 		$envData .= "ELASTICSERACH_AGGREGATE_SIZE = 100000" . PHP_EOL;
+	// 		$envData .= "ELASTICSERACH_EXTRACT_AGGREGATE_SIZE = 10000" . PHP_EOL;
 
-			if (fwrite($envFileHandle, $envData) !== false)
-			{
-				echo '.env file was created successfully.';
-			}
-			else
-			{
-				echo 'Error in writing to .env file';
-			}
-			fclose($envFileHandle);
-		}
-		else
-		{
-			echo 'Error in creating a handle for .env file';
-		}
+	// 		if (fwrite($envFileHandle, $envData) !== false)
+	// 		{
+	// 			echo '.env file was created successfully.';
+	// 		}
+	// 		else
+	// 		{
+	// 			echo 'Error in writing to .env file';
+	// 		}
+	// 		fclose($envFileHandle);
+	// 	}
+	// 	else
+	// 	{
+	// 		echo 'Error in creating a handle for .env file';
+	// 	}
 
-		$apacheConfFileHandle = fopen($apache_config_file, 'a+');
+	// 	$apacheConfFileHandle = fopen($apache_config_file, 'a+');
 
-		if ($apacheConfFileHandle !== false)
-		{
-			// Options None AllowOverride All  Require all granted </Directory>
-			$apacheConfData = PHP_EOL . '<Directory "/local/www/htdocs/' . $deployment_directory . '">' . PHP_EOL;
-			$apacheConfData .= "Options None" . PHP_EOL;
-			$apacheConfData .= "AllowOverride All" . PHP_EOL;
-			$apacheConfData .= "Require all granted" . PHP_EOL;
-			$apacheConfData .= "</Directory>";
+	// 	if ($apacheConfFileHandle !== false)
+	// 	{
+	// 		// Options None AllowOverride All  Require all granted </Directory>
+	// 		$apacheConfData = PHP_EOL . '<Directory "/local/www/htdocs/' . $deployment_directory . '">' . PHP_EOL;
+	// 		$apacheConfData .= "Options None" . PHP_EOL;
+	// 		$apacheConfData .= "AllowOverride All" . PHP_EOL;
+	// 		$apacheConfData .= "Require all granted" . PHP_EOL;
+	// 		$apacheConfData .= "</Directory>";
 
-			if (fwrite($apacheConfFileHandle, $apacheConfData) !== false)
-			{
-				echo 'Apache config file was modified successfully.';
-			}
-			else
-			{
-				echo 'Error in writing to Apache config file.';
-			}
-			fclose($apacheConfFileHandle);
-		}
-		else
-		{
-			echo 'Error in creating a handle for Apache config file';
-		}
-	}
+	// 		if (fwrite($apacheConfFileHandle, $apacheConfData) !== false)
+	// 		{
+	// 			echo 'Apache config file was modified successfully.';
+	// 		}
+	// 		else
+	// 		{
+	// 			echo 'Error in writing to Apache config file.';
+	// 		}
+	// 		fclose($apacheConfFileHandle);
+	// 	}
+	// 	else
+	// 	{
+	// 		echo 'Error in creating a handle for Apache config file';
+	// 	}
+	// }
 }
 
-if (isset($argc)) {
-	if($argc == 11){
-		$base_url = $argv[1];
-		$installation_key = $argv[2];
-		$php_bin_path = $argv[3];
-		$apache_config_file = $argv[4];
-		$deployment_directory = $argv[5];
-		$dbHost = $argv[6];
-		$dbRootUser = $argv[7];
-		$dbRootPassword = $argv[8];
-		$dbName = $argv[9];
-		$dbUsername = $argv[10];
+// if (isset($argc)) {
+// 	if($argc == 11){
+// 		$base_url = $argv[1];
+// 		$installation_key = $argv[2];
+// 		$php_bin_path = $argv[3];
+// 		$apache_config_file = $argv[4];
+// 		$deployment_directory = $argv[5];
+// 		$dbHost = $argv[6];
+// 		$dbRootUser = $argv[7];
+// 		$dbRootPassword = $argv[8];
+// 		$dbName = $argv[9];
+// 		$dbUsername = $argv[10];
 
-		$dbInfo = [
-			'host' => $dbHost,
-			'root_user' => $dbRootUser,
-			'root_password' => $dbRootPassword,
-			'db_name' => $dbName,
-			'username' => $dbUsername
-		];
+// 		$dbInfo = [
+// 			'host' => $dbHost,
+// 			'root_user' => $dbRootUser,
+// 			'root_password' => $dbRootPassword,
+// 			'db_name' => $dbName,
+// 			'username' => $dbUsername
+// 		];
 
-		CVInstaller::Deploy($base_url , $installation_key, $php_bin_path, $apache_config_file, $deployment_directory, $dbInfo);
-	}
-	else{
-		echo 'Incorrect number of arguments passed.';
-	}
-}
-else {
-	echo "argc and argv disabled\n";
-}
+// 		CVInstaller::Deploy($base_url , $installation_key, $php_bin_path, $apache_config_file, $deployment_directory, $dbInfo);
+// 	}
+// 	else{
+// 		echo 'Incorrect number of arguments passed.';
+// 	}
+// }
+// else {
+// 	echo "argc and argv disabled\n";
+// }
 
 
