877c0e50134da24f24bac5b9aefa8b36c3ad82f2
Sadegh Abadijou <s.abadijou@gmail.com>
2023-12-04 16:36:51 +0000

    Added redirecting to the home page once an error happens.

--- app/Views/errors/html/production.php
+++ app/Views/errors/html/production.php
@@ -1,25 +1,25 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="robots" content="noindex">
+A	<meta charset="UTF-8">
+	<meta name="robots" content="noindex">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<meta http-equiv="refresh" content="5;url=<?= base_url('/') ?>">
 
-    <title>Whoops!</title>
+	<title>Redirecting...</title>
 
-    <style type="text/css">
-        <?= preg_replace('#[\r\n\t ]+#', ' ', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'debug.css')) ?>
-    </style>
+	<style type="text/css">
+		<?= preg_replace('#[\r\n\t ]+#', ' ', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'debug.css')) ?>
+	</style>
 </head>
 <body>
-
-    <div class="container text-center">
-
-        <h1 class="headline">Whoops!</h1>
-
-        <p class="lead">We seem to have hit a snag. Please try again later...</p>
-
-    </div>
-
+<div class="container text-center">
+	<h1 class="headline">Whoops!</h1>
+	<p class="lead">We seem to have hit a snag. Please try again later...</p>
+	<h5>You will be redirected to the home page in 5 seconds.</h5>
+	<button type="button" class="btn btn-warning" onclick="history.back();">
+		Go Back
+	</button>
+</div>
 </body>
-
 </html>
