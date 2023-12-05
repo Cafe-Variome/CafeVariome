837148bd2edb2eb7802a672cd6ffb657f1c086d7
Sadegh Abadijou <s.abadijou@gmail.com>
2023-12-05 13:55:50 +0000

    Refactored NetworkInterface class to support New nexus server

--- app/Libraries/CafeVariome/Net/NetworkInterface.php
+++ app/Libraries/CafeVariome/Net/NetworkInterface.php
@@ -1,5 +1,8 @@
 <?php namespace App\Libraries\CafeVariome\Net;
 
+use App\Libraries\CafeVariome\Factory\AuthenticatorFactory;
+use App\Libraries\CafeVariome\Factory\SingleSignOnProviderAdapterFactory;
+
 /**
  * NetworkInterface.php
  *
@@ -7,6 +10,7 @@
  *
  * @author Mehdi Mehtarizadeh
  * @author Gregory Warren
+ * @auhtor Sadegh Abadijou
  *
  * This class interfaces the software with the authentication server and handles data sent/received to the server.
  * It is mainly for CRUD operation of shared entities.
@@ -14,153 +18,164 @@
 
 class NetworkInterface extends AbstractNetworkInterface
 {
-    protected $installation_key;
+	protected $installation_key;
 
-    public function __construct(string $targetUri = '') {
-        parent::__construct($targetUri);
+	public function __construct(string $targetUri = '', int $providerID = null) {
+		parent::__construct($targetUri, $providerID);
 
-        $this->installation_key = $this->setting->getInstallationKey();
+		$this->installation_key = $this->setting->getInstallationKey();
 
-        $this->serverURI = $this->setting->getAuthServerUrl();
-        $curlOptions = [CURLOPT_RETURNTRANSFER => TRUE,
-                        CURLOPT_HTTPHEADER => ['Expect:'] //Removing Expect header results in speed-up on some LAMPs running with CENTOS];
-        ];
+		$this->serverURI = $this->setting->getAuthServerUrl();
 
-        $this->networkAdapter = new cURLAdapter(null, $curlOptions);
+		$this->provider = (new SingleSignOnProviderAdapterFactory())->GetInstance()->Read($providerID);
 
-        if ($this->networkAdapterConfig->useProxy) {
-            $proxyDetails = $this->networkAdapterConfig->proxyDetails;
-            $this->configureProxy($proxyDetails);
-        }
-    }
+		$curlOptions = [CURLOPT_RETURNTRANSFER => TRUE,
+			CURLOPT_HTTPHEADER => ['Expect:'] //Removing Expect header results in speed-up on some LAMPs running with CENTOS];
+		];
 
-    protected function configureProxy(array $proxyConfig)
-    {
-        $this->networkAdapter->setOption(CURLOPT_FOLLOWLOCATION, true);
-        $this->networkAdapter->setOption(CURLOPT_HTTPPROXYTUNNEL, 1);
-        $this->networkAdapter->setOption(CURLOPT_PROXY, $proxyConfig['hostname']);
-        $this->networkAdapter->setOption(CURLOPT_PROXYPORT, $proxyConfig['port']);
+		$this->networkAdapter = new cURLAdapter(null, $curlOptions);
 
-        if ($proxyConfig['username'] != '' && $proxyConfig['password'] != '') {
-            $this->networkAdapter->setOption(CURLOPT_PROXYUSERPWD, $proxyConfig['username'] . ':' . $proxyConfig['password']);
-        }
-    }
+		if ($this->networkAdapterConfig->useProxy) {
+			$proxyDetails = $this->networkAdapterConfig->proxyDetails;
+			$this->configureProxy($proxyDetails);
+		}
+	}
 
-    public function CreateNetwork(array $data)
-    {
-        $this->adapterw('NetworkApi/createNetwork', $data);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+	protected function configureProxy(array $proxyConfig)
+	{
+		$this->networkAdapter->setOption(CURLOPT_FOLLOWLOCATION, true);
+		$this->networkAdapter->setOption(CURLOPT_HTTPPROXYTUNNEL, 1);
+		$this->networkAdapter->setOption(CURLOPT_PROXY, $proxyConfig['hostname']);
+		$this->networkAdapter->setOption(CURLOPT_PROXYPORT, $proxyConfig['port']);
 
-    public function AddInstallationToNetwork(array $data)
-    {
-        $this->adapterw('NetworkApi/addInstallationToNetwork', $data);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+		if ($proxyConfig['username'] != '' && $proxyConfig['password'] != '') {
+			$this->networkAdapter->setOption(CURLOPT_PROXYUSERPWD, $proxyConfig['username'] . ':' . $proxyConfig['password']);
+		}
+	}
 
-    public function GetNetworksByInstallationKey(string $installation_key)
-    {
-        $this->adapterw('NetworkApi/getNetworksByInstallationKey', ['installation_key' => $installation_key]);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+	public function CreateNetwork(array $data)
+	{
+		$this->adapterw('NetworkApi/createNetwork', ['installation_key' => $this->installation_key,
+			'network_name' => $data['network_name'], 'network_type' => $data['network_type'],
+			'network_threshold' => $data['network_threshold'], 'network_status' => $data['network_status']]);
 
-    public function GetNetwork(int $network_key)
-    {
-        $this->adapterw('NetworkApi/getNetwork', ['network_key' => $network_key]);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
 
-    public function GetNetworkThreshold(int $network_key)
-    {
-        $this->adapterw('NetworkApi/getNetworkThreshold', ['network_key' => $network_key]);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+	public function AddInstallationToNetwork(array $data)
+	{
+		$this->adapterw('NetworkApi/addInstallationToNetwork', ['installation_key' => $this->installation_key,
+			'network_key' => $data['network_key']]);
 
-    public function SetNetworkThreshold(int $network_key, int $network_threshold)
-    {
-        $this->adapterw('NetworkApi/setNetworkThreshold', ['network_key' => $network_key, 'network_threshold' => $network_threshold]);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
 
-    public function LeaveNetwork(int $network_key)
-    {
-        $this->adapterw('NetworkApi/leaveNetwork', ['network_key' => $network_key]);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+	public function GetNetworksByInstallationKey(string $installation_key)
+	{
+		$this->adapterw('NetworkApi/getNetworksByInstallationKey', ['installation_key' => $installation_key]);
 
-    public function GetAvailableNetworks()
-    {
-        $this->adapterw('NetworkApi/getAvailableNetworks', []);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
 
-    public function RequestToJoinNetwork(int $network_key, string $email, string $justification)
-    {
-        $this->adapterw('NetworkApi/requestToJoinNetwork', ['network_key' => $network_key, 'email' => $email, 'justification' => $justification]);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+	public function GetNetwork(int $network_key)
+	{
+		$this->adapterw('NetworkApi/getNetwork', ['installation_key' => $this->installation_key, 'network_key' => $network_key]);
 
-    public function AcceptRequest(string $token)
-    {
-        $this->adapterw('NetworkApi/acceptRequest', ['token' => $token]);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
 
-    public function DenyRequest(string $token)
-    {
-        $this->adapterw('NetworkApi/denyRequest', ['token' => $token]);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+	public function GetNetworkThreshold(int $network_key)
+	{
+		$this->adapterw('NetworkApi/getNetworkThreshold', ['installation_key' => $this->installation_key,
+			'network_key' => $network_key]);
 
-    public function GetInstallationsByNetworkKey(int $network_key)
-    {
-        $this->adapterw('NetworkApi/getInstallationsByNetworkKey', ['network_key' => $network_key]);
-        $response = $this->networkAdapter->Send();
-        return $this->processResponse($response);
-    }
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
 
-    protected function adapterw(string $uriTail, array $data)
-    {
-        $this->networkAdapter->setOption(CURLOPT_URL, $this->serverURI . $uriTail);
-        $this->networkAdapter->setOption(CURLOPT_POST, true);
-        $data['installation_key'] = $this->installation_key;
-        $this->networkAdapter->setOption(CURLOPT_POSTFIELDS, $data);
-    }
+	public function SetNetworkThreshold(int $network_key, int $network_threshold)
+	{
+		$this->adapterw('NetworkApi/setNetworkThreshold', ['installation_key' => $this->installation_key,
+			'network_key' => $network_key, 'network_threshold' => $network_threshold]);
 
-    // protected function processResponse($response)
-    // {
-    //     $responseObj = json_decode($response);
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
 
-    //     if ($responseObj == Null || !property_exists($responseObj, 'status')) {
-    //         $responseObj = new \StdClass();
-    //         $responseObj->status = false;
-    //     }
+	public function LeaveNetwork(int $network_key)
+	{
+		$this->adapterw('NetworkApi/leaveNetwork', ['installation_key' => $this->installation_key,
+			'network_key' => $network_key]);
 
-    //     return $responseObj;
-    // }
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
 
-    /**
-     * Checks availability of the authentication server before sending requests.
-     */
-    // public function ping(): bool
-    // {
-    //     $fp = @fsockopen($this->serverURI);
+	public function GetAvailableNetworks()
+	{
+		$this->adapterw('NetworkApi/getAvailableNetworks', ['installation_key' => $this->installation_key]);
 
-    //     if ($fp) {
-    //     //server is available
-    //     return true;
-    //     }
-    //     return false;
-    // }
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
+
+	public function RequestToJoinNetwork(int $network_key, string $email, string $justification)
+	{
+		$this->adapterw('NetworkApi/requestToJoinNetwork', ['installation_key' => $this->installation_key,
+			'network_key' => strval($network_key), 'email' => $email,
+			'justification' => $justification]);
+
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
+
+	public function AcceptRequest(string $token)
+	{
+		$this->adapterw('NetworkApi/acceptRequest', ['installation_key' => $this->installation_key,
+			'token' => $token]);
+
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
+
+	public function DenyRequest(string $token)
+	{
+		$this->adapterw('NetworkApi/denyRequest', ['installation_key' => $this->installation_key,
+			'token' => $token]);
+
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
+
+	public function GetInstallationsByNetworkKey(int $network_key)
+	{
+		$this->adapterw('NetworkApi/getInstallationsByNetworkKey', ['installation_key' => $this->installation_key,
+			'network_key' => $network_key]);
+
+		$response = $this->networkAdapter->Send();
+		return $this->processResponse($response);
+	}
+
+	protected function adapterw(string $uriTail, array $data)
+	{
+
+		$this->networkAdapter->setOption(CURLOPT_URL, $this->serverURI . $uriTail);
+		$this->networkAdapter->setOption(CURLOPT_POST, true);
+		$this->networkAdapter->setOption(CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $this->getAccessToken()));
+		$this->networkAdapter->setOption(CURLOPT_POSTFIELDS, $data);
+	}
+
+	public function getAccessToken()
+	{
+		$authenticatorFactory = new AuthenticatorFactory();
+
+		$authenticator = $authenticatorFactory->GetInstance($this->provider);
+
+		return $authenticator->getServiceAccessToken();
+
+	}
 }
