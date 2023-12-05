f425a8eaf6034daa781d0b9fb7a39fd724500b43
Sadegh Abadijou <s.abadijou@gmail.com>
2023-12-05 16:55:23 +0000

    CodeIgniter 4.4.x Upgrade: Added hot reload

--- app/Config/Events.php
+++ app/Config/Events.php
@@ -4,7 +4,7 @@ namespace Config;
 
 use CodeIgniter\Events\Events;
 use CodeIgniter\Exceptions\FrameworkException;
-
+use CodeIgniter\HotReloader\HotReloader;
 /*
  * --------------------------------------------------------------------
  * Application Events
@@ -23,28 +23,34 @@ use CodeIgniter\Exceptions\FrameworkException;
  */
 
 Events::on('pre_system', static function () {
-    if (ENVIRONMENT !== 'testing') {
-        if (ini_get('zlib.output_compression')) {
-            throw FrameworkException::forEnabledZlibOutputCompression();
-        }
+	if (ENVIRONMENT !== 'testing') {
+		if (ini_get('zlib.output_compression')) {
+			throw FrameworkException::forEnabledZlibOutputCompression();
+		}
 
-        while (ob_get_level() > 0) {
-            ob_end_flush();
-        }
+		while (ob_get_level() > 0) {
+			ob_end_flush();
+		}
 
-        ob_start(static fn ($buffer) => $buffer);
-    }
+		ob_start(static fn ($buffer) => $buffer);
+	}
 
-    /*
-     * --------------------------------------------------------------------
-     * Debug Toolbar Listeners.
-     * --------------------------------------------------------------------
-     * If you delete, they will no longer be collected.
-     */
-    if (CI_DEBUG && ! is_cli()) {
-        Events::on('DBQuery', 'CodeIgniter\Debug\Toolbar\Collectors\Database::collect');
-        Services::toolbar()->respond();
-    }
+	/*
+	 * --------------------------------------------------------------------
+	 * Debug Toolbar Listeners.
+	 * --------------------------------------------------------------------
+	 * If you delete, they will no longer be collected.
+	 */
+	if (CI_DEBUG && ! is_cli()) {
+		Events::on('DBQuery', 'CodeIgniter\Debug\Toolbar\Collectors\Database::collect');
+		Services::toolbar()->respond();
+
+		if (ENVIRONMENT === 'development') {
+			Services::routes()->get('__hot-reload', static function () {
+				(new HotReloader())->run();
+			});
+		}
+	}
 });
 
 Events::on('pre_system', '\App\Libraries\CafeVariome\CafeVariome::Boot');
