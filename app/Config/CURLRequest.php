cc99c737bd0b1f09f03dd83e6e25ae201a6a8953
Sadegh Abadijou <s.abadijou@gmail.com>
2023-12-05 16:54:28 +0000

    CodeIgniter 4.4.x Upgrade: Adjust configurations for CodeIgniter 4.4.x

--- app/Config/CURLRequest.php
+++ app/Config/CURLRequest.php
@@ -6,15 +6,15 @@ use CodeIgniter\Config\BaseConfig;
 
 class CURLRequest extends BaseConfig
 {
-    /**
-     * --------------------------------------------------------------------------
-     * CURLRequest Share Options
-     * --------------------------------------------------------------------------
-     *
-     * Whether share options between requests or not.
-     *
-     * If true, all the options won't be reset between requests.
-     * It may cause an error request with unnecessary headers.
-     */
-    public bool $shareOptions = true;
+	/**
+	 * --------------------------------------------------------------------------
+	 * CURLRequest Share Options
+	 * --------------------------------------------------------------------------
+	 *
+	 * Whether share options between requests or not.
+	 *
+	 * If true, all the options won't be reset between requests.
+	 * It may cause an error request with unnecessary headers.
+	 */
+	public bool $shareOptions = true;
 }
